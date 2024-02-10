12ab2afdcf27a6b2bf18d194f8f8689abe9cbd86
Lukrecio <Lucamegalodon@gmail.com>
2024-02-10 19:15:23 +0100

    Implementacion del tiempo, dentro de estos for es donde se ejecutará todo el código

---
+++ retosvarios/carrefour.java
@@ -0,0 +1,16 @@
+package retosvarios;
+
+class carrefour {
+    public static void main(String[] args) {
+
+        for(int hTime= 9; hTime<=21; hTime++){
+            for(int timeMinutes=0; timeMinutes <= 24; timeMinutes++){
+                System.out.println(timeMinutes);
+            }
+        }
+
+
+
+
+    }
+}
