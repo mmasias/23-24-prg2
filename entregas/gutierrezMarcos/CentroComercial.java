cec505ffdefc4055392be8968e9a382e844dfd6b
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2024-02-10 21:46:29 +0100

    Reto extendido hecho

--- entregas/gutierrezMarcos/CentroComercial.java
+++ entregas/gutierrezMarcos/CentroComercial.java
@@ -6,6 +6,10 @@ public class CentroComercial {
         int cola = 0;
         boolean[] cajaOcupada = new boolean[NUM_CAJAS];
         int[] itemsClienteCaja = new int[NUM_CAJAS];
+        int minutosSinCola = 0;
+        int personasEnColaAlFinalizar = 0;
+        int personasAtendidas = 0;
+        int itemsVendidos = 0;
 
         for (int minuto = 1; minuto <= MINUTOS_JORNADA; minuto++) {
             cleanScreen();
@@ -19,11 +23,14 @@ public class CentroComercial {
                 System.out.println("MINUTO " + minuto + " - Llega 1 persona - En Cola: " + cola);
             } else {
                 System.out.println("MINUTO " + minuto + " - Llegan 0 personas - En Cola: " + cola);
+                minutosSinCola++;
             }
 
             for (int i = 0; i < NUM_CAJAS; i++) {
                 if (cajaOcupada[i] && itemsClienteCaja[i] > 0) {
                     itemsClienteCaja[i]--;
+                    System.out.println("El cliente de la caja " + (i + 1) + " tiene un total de: " + itemsClienteCaja[i]);
+                    itemsVendidos++;
                 }
 
                 if (cola >= 1 && !cajaOcupada[i]) {
@@ -31,6 +38,8 @@ public class CentroComercial {
                     cola--;
                     cajaOcupada[i] = true;
                     itemsClienteCaja[i] = items();
+                    System.out.println("El cliente tiene un total de: " + itemsClienteCaja[i]);
+                    personasAtendidas++;
                 }
             }
 
@@ -38,9 +47,19 @@ public class CentroComercial {
 
             pause(2);
         }
+
+        personasEnColaAlFinalizar = cola;
+
+        System.out.println("\nEstadísticas Finales:");
+        System.out.println("Número de minutos en que no hubo nadie en cola: " + minutosSinCola);
+        System.out.println("Número de personas que estaban en cola al finalizar el día: " + personasEnColaAlFinalizar);
+        System.out.println("Número de personas atendidas durante el día: " + personasAtendidas);
+        System.out.println("Número de items vendidos en el día: " + itemsVendidos);
     }
 
+
     static void imprimirEstadoCajas(int[] itemsClienteCaja) {
+        System.out.print("Estado de las cajas: ");
         for (int i = 0; i < itemsClienteCaja.length; i++) {
             System.out.print("Caja" + (i + 1) + ":[" + itemsClienteCaja[i] + "] | ");
         }
