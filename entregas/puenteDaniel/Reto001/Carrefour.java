002feb7d9c298f4fac260bd10803938e04228825
Dani Puente <danielpuentesaranana@gmail.com>
2024-02-10 11:55:09 +0100

    Primer commit

---
+++ entregas/puenteDaniel/Reto001/Carrefour.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Carrefour{
+    public static void main(String[] args) {
+        int minuto, hora=0, nuevo;
+        int caja1=0, caja2=0, caja3=0, caja4=0, cola=0;
+        int ultimaHora=12, clientesAtendidos=0, productosVendidos=0, minutosSinCola=0;
+        float porcentaje = 0.4f;
+
+        boolean caja1Libre, caja2Libre, caja3Libre, caja4Libre;
+        boolean superAbierto = true;
+    }
+}
