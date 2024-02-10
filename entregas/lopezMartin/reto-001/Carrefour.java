54cda81d0f16687939dc6608b8b98aec22db2ceb
Martin <martin.lopez@alumnos.uneatlantico.es>
2024-02-10 13:48:26 +0100

    Probabilidad cambiada a 40% y añadida a una variable constante, es decir, tal y como viene explicado en la imagen del enunciado ya que considero que tiene mas sentido que un 60%.

--- entregas/lopezMartin/reto-001/Carrefour.java
+++ entregas/lopezMartin/reto-001/Carrefour.java
@@ -3,10 +3,11 @@ public class Carrefour {
     public static void main(String[] args) {
         
         final int MINUTOS_FINAL = 720;
+        final double probabilidadLlegadaClienhtes = 0.4;
         int colaLlegadaClientes = 0;
-
+        
         for(int minutoActual = 1; minutoActual<=MINUTOS_FINAL; minutoActual++){
-            if (Math.random()<=0.6) {
+            if (Math.random()<=probabilidadLlegadaClienhtes) {
                 colaLlegadaClientes++;
             }         
 
