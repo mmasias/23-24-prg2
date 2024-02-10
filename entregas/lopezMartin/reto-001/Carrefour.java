dd6f5607f6b6530bd9b333a62a6148f71442cab8
Martin <martin.lopez@alumnos.uneatlantico.es>
2024-02-10 13:42:22 +0100

    Creacion de gobernabilidad temporal del ejercicio y de la cola de llegada de clientes

--- entregas/lopezMartin/reto-001/Carrefour.java
+++ entregas/lopezMartin/reto-001/Carrefour.java
@@ -2,7 +2,15 @@ public class Carrefour {
 
     public static void main(String[] args) {
         
-        
+        final int MINUTOS_FINAL = 720;
+        int colaLlegadaClientes = 0;
+
+        for(int minutoActual = 1; minutoActual<=MINUTOS_FINAL; minutoActual++){
+            if (Math.random()<=0.6) {
+                colaLlegadaClientes++;
+            }         
+
+        }
 
     }
 }
