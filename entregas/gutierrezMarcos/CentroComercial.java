b12c1cd03d38111d034446fe3eedbb867f84ef0c
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2024-02-10 21:39:01 +0100

    Optimizacion del codigo de las cajas agregando las cajas en arrays

--- entregas/gutierrezMarcos/CentroComercial.java
+++ entregas/gutierrezMarcos/CentroComercial.java
@@ -1,145 +1,67 @@
 public class CentroComercial {
     public static void main(String[] args) {
-
         final int MINUTOS_JORNADA = 720;
+        final int NUM_CAJAS = 4;
 
         int cola = 0;
-        boolean caja1Ocupada = false;
-        int itemsClienteCaja1 = 0;
+        boolean[] cajaOcupada = new boolean[NUM_CAJAS];
+        int[] itemsClienteCaja = new int[NUM_CAJAS];
 
-        boolean caja2Ocupada = false;
-        int itemsClienteCaja2 = 0;
-
-        boolean caja3Ocupada = false;
-        int itemsClienteCaja3 = 0;
-
-        boolean caja4Ocupada = false;
-        int itemsClienteCaja4 = 0;
-
-        for(int minuto = 1;minuto <= MINUTOS_JORNADA; minuto++){
+        for (int minuto = 1; minuto <= MINUTOS_JORNADA; minuto++) {
             cleanScreen();
 
-            caja1Ocupada = caja1Estado(itemsClienteCaja1);
-            caja2Ocupada = caja2Estado(itemsClienteCaja2);
-            caja3Ocupada = caja3Estado(itemsClienteCaja3);
-            caja4Ocupada = caja4Estado(itemsClienteCaja4);
+            for (int i = 0; i < NUM_CAJAS; i++) {
+                cajaOcupada[i] = cajaEstado(itemsClienteCaja[i]);
+            }
 
-            if(llegaCliente()){
+            if (llegaCliente()) {
                 cola++;
                 System.out.println("Ha llegado un cliente!");
             }
 
             System.out.println("Hay una cola de: " + cola);
 
-            if(caja1Ocupada && itemsClienteCaja1 > 0){
-                itemsClienteCaja1--;
-                System.out.println("El cliente de la caja 1 tiene un total de: " + itemsClienteCaja1);
-            }
+            for (int i = 0; i < NUM_CAJAS; i++) {
+                if (cajaOcupada[i] && itemsClienteCaja[i] > 0) {
+                    itemsClienteCaja[i]--;
+                    System.out.println("El cliente de la caja " + (i + 1) + " tiene un total de: " + itemsClienteCaja[i]);
+                }
 
-            if(cola >= 1 && !caja1Ocupada){
-                System.out.println("Ha pasado a la caja 1 un cliente");
-                cola--;
-                caja1Ocupada = true;
-                itemsClienteCaja1 = items();
-                System.out.println("El cliente tiene un total de: " + itemsClienteCaja1);
-            }
-
-            if(caja2Ocupada && itemsClienteCaja2 > 0){
-                itemsClienteCaja2--;
-                System.out.println("El cliente de la caja 2 tiene un total de: " + itemsClienteCaja2);
-            }
-
-            if(cola >= 1 && !caja2Ocupada){
-                System.out.println("Ha pasado a la caja 1 un cliente");
-                cola--;
-                caja2Ocupada = true;
-                itemsClienteCaja2 = items();
-                System.out.println("El cliente tiene un total de: " + itemsClienteCaja2);
-            }
-
-            if(caja3Ocupada && itemsClienteCaja3 > 0){
-                itemsClienteCaja3--;
-                System.out.println("El cliente de la caja 3 tiene un total de: " + itemsClienteCaja3);
-            }
-
-            if(cola >= 1 && !caja3Ocupada){
-                System.out.println("Ha pasado a la caja 1 un cliente");
-                cola--;
-                caja3Ocupada = true;
-                itemsClienteCaja3 = items();
-                System.out.println("El cliente tiene un total de: " + itemsClienteCaja3);
-            }
-
-            if(caja4Ocupada && itemsClienteCaja4 > 0){
-                itemsClienteCaja4--;
-                System.out.println("El cliente de la caja 4 tiene un total de: " + itemsClienteCaja4);
-            }
-
-            if(cola >= 1 && !caja4Ocupada){
-                System.out.println("Ha pasado a la caja 1 un cliente");
-                cola--;
-                caja4Ocupada = true;
-                itemsClienteCaja4 = items();
-                System.out.println("El cliente tiene un total de: " + itemsClienteCaja4);
+                if (cola >= 1 && !cajaOcupada[i]) {
+                    System.out.println("Ha pasado a la caja " + (i + 1) + " un cliente");
+                    cola--;
+                    cajaOcupada[i] = true;
+                    itemsClienteCaja[i] = items();
+                    System.out.println("El cliente tiene un total de: " + itemsClienteCaja[i]);
+                }
             }
 
             pause(2);
-
-        }
-
-    }
-
-    static boolean caja1Estado(int items){
-        if(items == 0){
-            return false;
-        }else{
-            return true;
         }
     }
 
-    static boolean caja2Estado(int items){
-        if(items == 0){
-            return false;
-        }else{
-            return true;
-        }
+    static boolean cajaEstado(int items) {
+        return items > 0;
     }
 
-    static boolean caja3Estado(int items){
-        if(items == 0){
-            return false;
-        }else{
-            return true;
-        }
-    }
-
-    static boolean caja4Estado(int items){
-        if(items == 0){
-            return false;
-        }else{
-            return true;
-        }
-    }
-
-    static int items(){
+    static int items() {
         return (int) (Math.random() * 11) + 5;
     }
 
-    static boolean llegaCliente(){
+    static boolean llegaCliente() {
         return Math.random() <= 0.6;
     }
 
     static void cleanScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
-      }
+    }
 
-      
     static void pause(int seconds) {
         try {
             Thread.sleep(1000 * seconds);
         } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
-
 }
