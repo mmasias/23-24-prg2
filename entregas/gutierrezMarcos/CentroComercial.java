36637c62b96189f66110b5747b1eb1b04050de5c
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2024-02-10 21:22:33 +0100

    Jornada en minutos

--- entregas/gutierrezMarcos/CentroComercial.java
+++ entregas/gutierrezMarcos/CentroComercial.java
@@ -1,6 +1,11 @@
 public class CentroComercial {
     public static void main(String[] args) {
 
+        final int MINUTOS_JORNADA = 720;
+
+        for(int minuto = 1;minuto <= MINUTOS_JORNADA; minuto++){
+            
+        }
     }
 
     static boolean llegaCliente(){
