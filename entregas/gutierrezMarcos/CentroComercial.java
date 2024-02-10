33a89e57dfc076daacfbbdc2715de0dc94b9053a
marcosgutierrez6 <144948806+marcosgutierrez6@users.noreply.github.com>
2024-02-10 21:36:35 +0100

    Implementacion de las 4 cajas con su logica

--- entregas/gutierrezMarcos/CentroComercial.java
+++ entregas/gutierrezMarcos/CentroComercial.java
@@ -7,10 +7,22 @@ public class CentroComercial {
         boolean caja1Ocupada = false;
         int itemsClienteCaja1 = 0;
 
+        boolean caja2Ocupada = false;
+        int itemsClienteCaja2 = 0;
+
+        boolean caja3Ocupada = false;
+        int itemsClienteCaja3 = 0;
+
+        boolean caja4Ocupada = false;
+        int itemsClienteCaja4 = 0;
+
         for(int minuto = 1;minuto <= MINUTOS_JORNADA; minuto++){
             cleanScreen();
 
             caja1Ocupada = caja1Estado(itemsClienteCaja1);
+            caja2Ocupada = caja2Estado(itemsClienteCaja2);
+            caja3Ocupada = caja3Estado(itemsClienteCaja3);
+            caja4Ocupada = caja4Estado(itemsClienteCaja4);
 
             if(llegaCliente()){
                 cola++;
@@ -32,6 +44,45 @@ public class CentroComercial {
                 System.out.println("El cliente tiene un total de: " + itemsClienteCaja1);
             }
 
+            if(caja2Ocupada && itemsClienteCaja2 > 0){
+                itemsClienteCaja2--;
+                System.out.println("El cliente de la caja 2 tiene un total de: " + itemsClienteCaja2);
+            }
+
+            if(cola >= 1 && !caja2Ocupada){
+                System.out.println("Ha pasado a la caja 1 un cliente");
+                cola--;
+                caja2Ocupada = true;
+                itemsClienteCaja2 = items();
+                System.out.println("El cliente tiene un total de: " + itemsClienteCaja2);
+            }
+
+            if(caja3Ocupada && itemsClienteCaja3 > 0){
+                itemsClienteCaja3--;
+                System.out.println("El cliente de la caja 3 tiene un total de: " + itemsClienteCaja3);
+            }
+
+            if(cola >= 1 && !caja3Ocupada){
+                System.out.println("Ha pasado a la caja 1 un cliente");
+                cola--;
+                caja3Ocupada = true;
+                itemsClienteCaja3 = items();
+                System.out.println("El cliente tiene un total de: " + itemsClienteCaja3);
+            }
+
+            if(caja4Ocupada && itemsClienteCaja4 > 0){
+                itemsClienteCaja4--;
+                System.out.println("El cliente de la caja 4 tiene un total de: " + itemsClienteCaja4);
+            }
+
+            if(cola >= 1 && !caja4Ocupada){
+                System.out.println("Ha pasado a la caja 1 un cliente");
+                cola--;
+                caja4Ocupada = true;
+                itemsClienteCaja4 = items();
+                System.out.println("El cliente tiene un total de: " + itemsClienteCaja4);
+            }
+
             pause(2);
 
         }
@@ -39,6 +90,30 @@ public class CentroComercial {
     }
 
     static boolean caja1Estado(int items){
+        if(items == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    static boolean caja2Estado(int items){
+        if(items == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    static boolean caja3Estado(int items){
+        if(items == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    static boolean caja4Estado(int items){
         if(items == 0){
             return false;
         }else{
