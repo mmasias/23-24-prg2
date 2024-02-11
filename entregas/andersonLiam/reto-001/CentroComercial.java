05f942232947d501b3c091ec4d4a87f95535087c
liamanderson873 <liam.anderson@alumnos.uneatlantico>
2024-02-11 18:48:15 +0100

    commit inicial

---
+++ entregas/andersonLiam/reto-001/CentroComercial.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class CCF {
+    private static final int HORAS_TRABAJO = 12;
+    private static final int INTERVALO_TIEMPO = 5; 
+    private static final int CAJAS = 4;
+    private static final int MIN_PACKS = 5;
+    private static final int MAX_PACKS = 15;
+    private static final double PROBABILIDAD_LLEGADA = 0.6;
+
+  
