abc6ae2b8f96443cf7ccf47cc1e346d2d82ff2b5
nicolas.lopez <nicolas.lopez@alumnos.uneatlantico.es>
2024-02-12 13:06:11 +0100

    Primeras variables + main

---
+++ entregas/LopezNicolas/reto_001/CCCF.java
@@ -0,0 +1,11 @@
+public class CCCF {
+
+    static double arrival_prob = 0.4;
+    static int items_min = 5;
+    static int items_max = 15;
+
+    public static void main(String[] args) {
+
+    }
+
+}
