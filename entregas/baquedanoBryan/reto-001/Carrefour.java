32372a86e905a5b95b2328cdd6f83305ed76cefc
enmabry <bryanbaquedano11@gmail.com>
2024-02-12 21:54:56 +0100

    Nuevo arreglo de items y condicional que controla los items
    Lo que hice fue agregar una nueva matriz que le va a agregar items a una caja especifica, cuando los items lleguen a 0, la caja se reinicia a 0 haciendo que pueda aceptar otro cliente, aparte de esto agreguè que se pudiera imprimir los numero de items que se van cobrando en cada iteraciòn

--- entregas/baquedanoBryan/reto-001/Carrefour.java
+++ entregas/baquedanoBryan/reto-001/Carrefour.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 class Carrefour {
     public static void main(String[] args) {
-        // Scanner entrada = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         final int TOTAL_MINUTES_PER_DAY = 720;
         int minutesWorked = 0;
         final double PROBABILITY = 0.6;
         int waitingLineCostumer = 0;
         int[] waitingLinePerCashier = { 0, 0, 0, 0 };
+        int[] itemsRemainingPerCashier = { 0, 0, 0, 0 };
 
         do {
+
             double costumerProbability = Math.random();
             minutesWorked++;
             waitingLineCostumer = costumerArrival(PROBABILITY, costumerProbability, waitingLineCostumer);
@@ -18,17 +20,33 @@ class Carrefour {
             for (int i = 0; i < waitingLinePerCashier.length && waitingLineCostumer > 0; i++) {
                 if (waitingLinePerCashier[i] == 0) {
                     waitingLinePerCashier[i]++;
+                    itemsRemainingPerCashier[i] = generateRandomItems();
                     waitingLineCostumer--;
+                }else if (itemsRemainingPerCashier[i] > 0) {
+                    itemsRemainingPerCashier[i]--;
+                    if (itemsRemainingPerCashier[i] == 0) {
+                        waitingLinePerCashier[i] = 0;
+                    }
                 }
             }
 
-            System.out.println("Minuto " + minutesWorked);
-            for (int i = 0; i < waitingLinePerCashier.length; i++) {
-                System.out.println("Caja " + (i + 1) + ": " + waitingLinePerCashier[i] + " cliente(s)");
-            }
-            System.out.println("Clientes en espera: " + waitingLineCostumer);
-
+            imprimirEstadoCajas(minutesWorked, waitingLinePerCashier,itemsRemainingPerCashier, waitingLineCostumer);
+            scanner.nextLine();
         } while (minutesWorked < TOTAL_MINUTES_PER_DAY);
+
+    }
+
+    private static int generateRandomItems() {
+        return (int) (Math.random() * 11) + 5;
+    }
+
+    private static void imprimirEstadoCajas(int minutesWorked, int[] waitingLinePerCashier,int[] itemsRemainingPerCashier, int waitingLineCostumer) {
+        System.out.println("Minuto " + minutesWorked);
+        for (int i = 0; i < waitingLinePerCashier.length; i++) {
+            System.out.println("Caja " + (i + 1) + ": " + waitingLinePerCashier[i] + " cliente(s) en espera con " +
+                    itemsRemainingPerCashier[i] + " item(s) restante(s)");
+        }
+        System.out.println("Clientes en espera: " + waitingLineCostumer);
 
     }
 
@@ -38,4 +56,5 @@ class Carrefour {
         }
         return waitingLineCostumer;
     }
+
 }
