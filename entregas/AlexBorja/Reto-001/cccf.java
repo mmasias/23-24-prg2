f4b1d618e41be0d2e74a830817cd7aaff46de1bc
alexborja <gersonperezgerder@gmail.com>
2024-02-12 20:33:46 +0100

    adding variables

---
+++ entregas/AlexBorja/Reto-001/cccf.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Cccf {
+   static int totalPersonsInDay = 0;
+   static int totalItemsSold = 0;
+
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      final int TOTAL_MINUTES = 720;
+      final double NEW_COSTUMER_PROB = 0.6;
+      final int MIN_ITEMS = 5;
+      final int MAX_ITEMS = 15;
+
+      int line = 0;
+      int noClients = 0;
+      int currentTime = 0;
+      boolean is_open = true;
+      int[] cashiers = { 0, 0, 0, 0, 0, 0};
+
+
+   }
+
+}
