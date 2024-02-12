c540e88bcc74ca42038109a1d0f574b07daa1d84
JPMorales04 <jpmorales.epic@gmail.com>
2024-02-12 21:07:45 +0100

    Se agrega una caja
    se agrega una caja y ahora la cantidad de clientes que aparece en la cola durante cada hora es random

--- entregas/MoralesJosePablo/reto-001/Carrefour.java
+++ entregas/MoralesJosePablo/reto-001/Carrefour.java
@@ -13,6 +13,9 @@ class Carrefour {
 
         int cola = 0;
         final double PROBABILIDAD_LLEGADA_CLIENTES = 0.6;
+        int caja = 0;
+        final int MAX_ITEMS_CLIENTES = 15;
+        final int MIN_ITEMS_CLIENTES = 5;
 
         do {
             atendiendoclientes += DURACION_ATENCION;
@@ -21,6 +24,17 @@ class Carrefour {
             if (Math.random() < PROBABILIDAD_LLEGADA_CLIENTES) {
                 cola = cola + 1;
             }
+
+            if (caja <= 0) {
+                cola = cola + 1;
+                caja = (int) (Math.random() * (MAX_ITEMS_CLIENTES - MIN_ITEMS_CLIENTES)) +MIN_ITEMS_CLIENTES;
+                
+            }
+            if (caja > 0) {
+                caja = caja -1;
+                
+            }
+            
             System.out.print("Clientes en la cola: " + cola + "\n");
             scanner.nextLine();
 
