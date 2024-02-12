58e1756a0ca8cfb8c93343f7965383ab889caae5
jaimelinares6 <jaime.linares@alumnos.uneatlantico.es>
2024-02-12 21:39:51 +0100

    Commit inicial(variables)

---
+++ CentroComercial.java
@@ -0,0 +1,15 @@
+public class CentroComercial {
+
+    private static final int NUMERO_CAJAS = 4;
+    private static final double PROBABILIDAD_LLEGADA_CLIENTE = 0.6;
+
+    public static void main(String[] args) {
+        int[] colaClientes = new int[720];
+
+        int[] tiempoAtencionCajas = new int[NUMERO_CAJAS];
+        int minutosConColaCero = 0;
+        int personasEnColaAlCierre = 0;
+        int personasAtendidas = 0;
+        int articulosVendidos = 0;
+    }
+}
