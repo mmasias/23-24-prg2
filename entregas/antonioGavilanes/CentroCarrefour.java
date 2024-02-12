c9c2eed659f419c3ff6e8730e7d612e067fbd331
Antonio Gavilanes <antonio.gavilanes@alumnos.uneatlantico.es>
2024-02-12 11:37:36 +0100

    Primeras implementaciones

---
+++ entregas/antonioGavilanes/CentroCarrefour.java
@@ -0,0 +1,17 @@
+
+public class CentroCarrefour {
+    static double probabilidad = 0.6;
+    static int cajas = 4;
+    static int minPack = 5;
+    static int maxPack = 15;
+    static int tiempoInicio = 9;
+    static int tiempoFinal = 21;
+
+    public static void main(String[] args) {
+        for (int i = tiempoInicio; i <= tiempoFinal; i++) {
+            System.out.println("hora del dia " + i);
+        }
+
+        
+    }
+}
