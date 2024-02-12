061172de800d237dd14bc4b1e684fa2b4f4816ea
mariiooo <mario.viadero@alumnos.uneatlantico.es>
2024-02-12 21:47:54 +0100

    desarrollo del funcionamiento de las cajas y los items

--- entregas/viaderoMario/Reto001.java
+++ entregas/viaderoMario/Reto001.java
@@ -11,6 +11,77 @@ public class Reto001 {
     boolean newPerson = false;
     int timeWithoutQueque = 0;
     int totalItems = 0;
+    
+    for (int time = 1; time < minutesPerDay; time++) {
+      boolean activa = true;
+      if (Math.random() * 100 < 40) {
+        newPerson = true;
+        queque++; 
+        if (free[0]) {
+          box[0]++;
+          free[0] = false; 
+          items[0] = ((int) (Math.random() * 100)) % 11 + 5;
+          totalItems = totalItems + items[0];
+          queque--;
+        } else if (free[1]) {
+          box[1]++;
+          free[1] = false;
+          items[1] = ((int) (Math.random() * 100)) % 11 + 5;
+          totalItems = totalItems + items[1];
+          queque--;
+        } else if (free[2]) {
+          box[2]++;
+          free[2] = false; 
+          items[2] = ((int) (Math.random() * 100)) % 11 + 5;
+          totalItems = totalItems + items[2];
+          queque--;
+        } else if (free[3]) {
+          box[3]++;
+          free[3] = false;
+          items[3] = ((int) (Math.random() * 100)) % 11 + 5;
+          totalItems = totalItems + items[3];
+          queque--;
+        } else if (free[4] && activa) {
+          box[4]++;
+          free[4] = false; 
+          items[4] = ((int) (Math.random() * 100)) % 11 + 5;
+          totalItems = totalItems + items[4];
+          queque--;
+        }
+      }
+      if (items[0] > 0) {
+        items[0]--;
+        if (items[0] == 0) {
+          free[0] = true;
+        }
+      }
+      if (items[1] > 0) {
+        items[1]--;
+        if (items[1] == 0) {
+          free[1] = true;
+        }
+      }
+      if (items[2] > 0) {
+        items[2]--;
+        if (items[2] == 0) {
+          free[2] = true;
+        }
+      }
+      if (items[3] > 0) {
+        items[3]--;
+        if (items[3] == 0) {
+          free[3] = true;
+        }
+      }
+      if (items[4] > 0) {
+        items[4]--;
+        if (items[4] == 0) {
+          free[4] = true;
+        }
+      }
+      if (queque == 0) {
+        timeWithoutQueque++;
 
+  
   }
 }
