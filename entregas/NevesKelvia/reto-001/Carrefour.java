8be4e04e6985f66b0037f01ea0c6af1113a439d9
kelvia.barros <barroskelvia122@gmail.com>
2024-02-12 02:23:02 +0100

    Introduction of the line, the cashier and the items (max & min)

--- entregas/NevesKelvia/reto-001/Carrefour.java
+++ entregas/NevesKelvia/reto-001/Carrefour.java
@@ -7,13 +7,30 @@ class Carrefour {
         int time = 0;
         boolean isOpen = true;
         final double PROBABILITY_ARRIVAL = 0.6;
+        int cashier = 0;
+        final int ITEM_MINIMUM = 5;
+        final int ITEM_MAXIMUM = 15;
 
-        if (time<CLOSING_TIME) {
+        if (time < CLOSING_TIME) {
             isOpen = true;
             time++;
             System.out.println("Time: " + time);
         }
-       
+        int line = 0;
 
-}
+        line = line + (Math.random() < PROBABILITY_ARRIVAL ? 1 : 0);
+
+        System.out.println(time);
+        System.out.println("Line: " + line);
+
+        if (cashier <= 0) {
+            line = line - 1;
+            cashier = (int) (Math.random() * (ITEM_MAXIMUM - ITEM_MINIMUM)) + ITEM_MINIMUM;
+        }
+
+        if (cashier > 0) {
+            cashier = cashier - 1;
+        }
+
+    }
 }
