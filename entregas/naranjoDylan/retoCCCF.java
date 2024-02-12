1a8e854bedf160af28f016a9e3eb886585b7e8d7
DylanNRJ <dylannaranjo53@gmail.com>
2024-02-12 13:49:09 +0100

    primer commit donde determine todas las variables a necesitar

---
+++ entregas/naranjoDylan/retoCCCF.java
@@ -0,0 +1,26 @@
+class retoCCCF {
+    public static void main(string[] args) {
+        int tiempototal;
+        double probabilidadLlegada;
+        int unaCola;
+        int caja1;
+        int caja2;
+        int caja3;
+        int caja4;
+        int colaencero;
+        int packstotal;
+        int colatotal;
+        int packs;
+
+        tiempototal = 720;
+        colaencero = 0;
+        unacola = 0;
+        caja1 = 0;
+        caja2 = 0;
+        caja3 = 0;
+        caja4 = 0;
+        packstotal = 0;
+        colatotal = 0;
+        packs = 0;
+    }
+}
