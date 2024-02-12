ecbc5634286ba6b5a74db09a1ba5c833dbf9dfb7
liamanderson873 <liam.anderson@alumnos.uneatlantico>
2024-02-12 19:28:21 +0100

    if para las cajas

--- entregas/andersonLiam/reto-001/CentroComercial.java
+++ entregas/andersonLiam/reto-001/CentroComercial.java
@@ -12,10 +12,53 @@ public class CentroComercial {
         int minutosSinCola = 0;
         int totalItems = 0;
         boolean activa = false;
-        
 
-       
+        for (int tiempo = 1; tiempo < minutosEnUnDia; tiempo++) {
+
+            if (Math.random() * 100 < 40) {
+                llegaNuevo = true;
+                cola++;
+                if (libre[0]) {
+                    caja[0]++;
+                    libre[0] = false;
+                    items[0] = ((int) (Math.random() * 100)) % 11 + 5;
+                    totalItems = totalItems + items[0];
+
+                    cola--;
+
+                } else if (libre[1]) {
+                    caja[1]++;
+                    libre[1] = false;
+                    items[1] = ((int) (Math.random() * 100)) % 11 + 5;
+                    totalItems = totalItems + items[1];
+                    cola--;
+
+                } else if (libre[2]) {
+                    caja[2]++;
+                    libre[2] = false;
+                    items[2] = ((int) (Math.random() * 100)) % 11 + 5;
+                    totalItems = totalItems + items[2];
+                    cola--;
+
+                } else if (libre[3]) {
+                    caja[3]++;
+                    libre[3] = false;
+                    items[3] = ((int) (Math.random() * 100)) % 11 + 5;
+                    totalItems = totalItems + items[3];
+                    cola--;
+                } else if (libre[4] && activa) {
+                    caja[4]++;
+                    libre[4] = false;
+                    items[4] = ((int) (Math.random() * 100)) % 11 + 5;
+                    totalItems = totalItems + items[4];
+                    cola--;
+
+                }
+
+            }
+
+        }
+
     }
-    
-  
+
 }
