b599cc8e1b78fdaaa0cc2d42a75d378b30f8ff80
brendalopes <brenda14lopess@gmail.com>
2024-02-13 00:15:25 +0100

    commit 4

--- entregas/lopesbrenda/reto001/CentroComercial.java
+++ entregas/lopesbrenda/reto001/CentroComercial.java
@@ -4,39 +4,31 @@ import java.util.Random;
 
 public class CentroComercial {
     public static void main(String[] args) {
-        final int HORAS_DE_TRABAJO = 12;
         final double PROBABILIDAD_LLEGADA_CLIENTE = 0.40;
+        final int MIN_PACKS = 5;
+        final int MAX_PACKS = 15;
 
         Random random = new Random();
         int[] cajas = new int[4];
         int clientesEnCola = 0;
 
-        for (int hora = 0; hora < HORAS_DE_TRABAJO; hora++) {
-            System.out.println("Hora " + hora + ":");
 
             for (int minuto = 0; minuto < 60; minuto++) {
-                if (random.nextDouble() < PROBABILIDAD_LLEGADA_CLIENTE) {
-                    clientesEnCola++;
-                }
-                for (int i = 0; i < 4; i++) {
-                    if (cajas[i] > 0) {
-                        cajas[i]--;
-                    } else if (clientesEnCola > 0) {
-                        int packs = 5 + random.nextInt(15 - 5 + 1);
-                        cajas[i] = packs * 1;
-                        clientesEnCola--;
-                    }
+                System.out.println("MINUTO " + (minuto + 1) + " - " + (random.nextDouble() < PROBABILIDAD_LLEGADA_CLIENTE ? "Llega 1 persona" : "No llega nadie") + " - En Cola: " + clientesEnCola);
+
+               for (int i = 0; i < 4; i++) {
+                if (cajas[i] > 0) {
+                    cajas[i]--;
+                } else if (clientesEnCola > 0) {
+                    int packs = MIN_PACKS + random.nextInt(MAX_PACKS - MIN_PACKS + 1);
+                    cajas[i] = packs * 1;
+                    clientesEnCola--;
                 }
             }
 
-            for (int i = 0; i < 4; i++) {
-                if (cajas[i] > 0) {
-                    System.out.println("Caja " + (i + 1) + ": atendiendo (" + cajas[i] + " packs restantes)");
-                } else {
-                    System.out.println("Caja " + (i + 1) + ": libre");
-                }
-            }
-            System.out.println("Personas en cola: " + clientesEnCola);
+            System.out.print("Caja1:[" + cajas[0] + "] | Caja2:[" + cajas[1] + "] | Caja3:[" + cajas[2] + "] | Caja4:[" + cajas[3] + "]\n");
+            System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
         }
     }
 }
