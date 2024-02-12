8b15ab69e0744dfbe2bbfa85d829dcef740122e8
Antonio Gavilanes <antonio.gavilanes@alumnos.uneatlantico.es>
2024-02-13 00:34:52 +0100

    Clientes y cajas

--- entregas/antonioGavilanes/CentroCarrefour.java
+++ entregas/antonioGavilanes/CentroCarrefour.java
@@ -1,11 +1,13 @@
-import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Queue;
 import java.util.Random;
 
 public class CentroCarrefour {
-    static double probabilidad = 0.6;
-    static int cajas = 4;
+    static int clientes = 0;
+    static String mensaje;
+    static int caja1 = 0;
+    static int caja2 = 0;
+    static int caja3 = 0;
+    static int caja4 = 0;
     static int minPack = 5;
     static int maxPack = 15;
     static int tiempoInicio = 9;
@@ -18,7 +20,13 @@ public class CentroCarrefour {
             imprimirMarcoHora();
             for (int minutos = 0; minutos < 60; minutos++) {
                 imprimirMarcoMinutos();
-                System.out.println("Hora: " + hora + ":" + String.format("%02d", minutos));
+                clientesEnCaja();
+
+                System.out.println("Hora: " + hora + ":" + String.format("%02d", minutos) + " || " + mensaje
+                        + " || Clientes en cola: " + clientes);
+                System.out.println("Caja 1: " + "[" + caja1 + "]" + " productos" + " || Caja 2: " + "[" + caja2 + "]"
+                        + " productos" + " || Caja 3: " + "[" + caja3 + "]" + " productos" + " || Caja 4: " + "["
+                        + caja4 + "]" + " productos");
                 sc.nextLine();
 
             }
@@ -26,21 +34,64 @@ public class CentroCarrefour {
         sc.close();
     }
 
+    public static void clientesEnCaja() {
+        Random clientesRandom = new Random();
+        Random packItems = new Random();
+        int items = packItems.nextInt(maxPack - minPack + 1) + minPack;
+        if (clientesRandom.nextDouble() < 0.6) {
+            clientes = clientes + 1;
+            mensaje = "Llega un cliente";
+        } else {
+            mensaje = "No ha llegado nadie";
+        }
+
+        if (clientes > 0 && caja1 < 15 && caja1 + items <= 15) {
+            mensaje = "Un cliente ha sido atendido";
+            caja1 = caja1 + items;
+        } else if (clientes > 0 && caja2 < 15 && caja2 + items <= 15) {
+            mensaje = "Un cliente ha sido atendido";
+            caja2 = caja2 + items;
+        }
+
+        else if (clientes > 0 && caja3 < 15 && caja3 + items <= 15) {
+            mensaje = "Un cliente ha sido atendido";
+            caja3 = caja3 + items;
+        }
+
+        else if (clientes > 0 && caja4 < 15 && caja4 + items <= 15) {
+            mensaje = "Un cliente ha sido atendido";
+            caja4 = caja4 + items;
+        }
+
+        if (caja1 > 0) {
+            caja1 = caja1 - 1;
+        }
+        if (caja2 > 0) {
+            caja2 = caja2 - 1;
+        }
+        if (caja3 > 0) {
+            caja3 = caja3 - 1;
+        }
+        if (caja4 > 0) {
+            caja4 = caja4 - 1;
+        }
+    }
+
     public static void imprimirMarcoMinutos() {
         System.out.print("+");
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 35; i++) {
             System.out.print("-");
         }
         System.out.println("+");
     }
 
     public static void imprimirMarcoHora() {
-        for (int i = 0; i < 25; i++) {
+        for (int i = 0; i < 40; i++) {
             System.out.print("=");
         }
         System.out.println();
         System.out.println("Centro Comercial Carrefour");
-        for (int i = 0; i < 25; i++) {
+        for (int i = 0; i < 40; i++) {
             System.out.print("=");
         }
         System.out.println();
