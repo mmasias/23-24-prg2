ae99528b056c56115afd1f6bbb2503124e087dc9
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2024-02-12 12:41:28 +0100

    Vuelve a 0
    Debido a que el codigo llego un momento en el que sentia que no se podia arreglar y no era del todo legible, lo borré e iniciaré desde 0

--- entregas/juarezAlejandro/reto-001/README.md
+++ entregas/juarezAlejandro/reto-001/README.md
@@ -1,2 +1,4 @@
 Por el momento lo intentaré en español, luego lo iré cambiando a inglés
 Si en algun momento, parece que su código lo copié es porque muchas veces necesitaba darme una idea de como seguir. Además que no se porque en el anterior commit parece que es su código el que subí cuando realmente, los cambios me aparecian guardados con mi código.
+
+Debido a que el codigo en un momento sentia que no se entendía y no era lo suficientemente legible, mejor opte por volver a iniciarlo todo de 0, para que se pueda leer de mejor manera (además que ahora lo estoy intentando repasar con un esquema de flujo).
--- entregas/juarezAlejandro/reto-001/carrefour.java
+++ entregas/juarezAlejandro/reto-001/carrefour.java
@@ -2,33 +2,7 @@ import java.util.Scanner;
 
 class Carrefour {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        final int HORA_APERTURA = 9;
-        final int HORA_CIERRE = 21;
-        final int TOTAL_TIEMPO_ABIERTO = 720;
-        final double PROBABILIDAD_LLEGADA_CLIENTE = 0.6;
-
-        boolean cajaAbierta = false;
-
-        for (int tiempoAbierto = 0; tiempoAbierto <= TOTAL_TIEMPO_ABIERTO; tiempoAbierto++) {
-            int horas = (HORA_APERTURA + tiempoAbierto / 60) % 24;
-            int minutos = tiempoAbierto % 60;
-            scanner.nextLine();
-            System.out.println(horas + ":" + minutos);
-            double probabilidadLlegada = Math.random();
-            if (probabilidadLlegada <= PROBABILIDAD_LLEGADA_CLIENTE) {
-                System.out.println("Llega un cliente");
-                if (!cajaAbierta){
-                    System.out.println("Caja abierta para que pase un cliente");
-                    cajaAbierta = true;
-                    int itemsCliente = (int) (Math.random() * 11) + 5;
-                    tiempoAbierto += itemsCliente;
-                    System.out.println("El cliente lleva " + itemsCliente + " items");
-                    cajaAbierta = false;
-                }
-            }
-
+            Scanner scanner = new Scanner(System.in);
+            scanner.close();
         }
-        scanner.close();
-    }
 }
