1facb193470b00a26b15558faebb684134317d04
enmabry <bryanbaquedano11@gmail.com>
2024-02-12 18:45:55 +0100

    Meotodo de iteracion

---
+++ entregas/baquedanoBryan/reto-001/Carrefour.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Carrefour {
+    public static void main(String[] args) {
+       // Scanner entrada = new Scanner(System.in);
+
+        final int TOTAL_MINUTES_PER_DAY = 720;
+        int minutesWorked = 0;
+
+
+
+       do {
+        minutesWorked = minutesWorked++;
+        
+        
+       } while (minutesWorked<TOTAL_MINUTES_PER_DAY);
+
+    }
+}
