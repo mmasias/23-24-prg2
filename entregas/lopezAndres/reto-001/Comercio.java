d3ffa92c37a50e96dccd6bd138ed8dc784bb67af
andreslb9 <144810780+andreslb9@users.noreply.github.com>
2024-02-12 22:55:03 +0100

    Segundo Commit

--- entregas/lopezAndres/reto-001/Comercio.java
+++ entregas/lopezAndres/reto-001/Comercio.java
@@ -15,6 +15,32 @@ public class Comercio {
         int personasEnColaAlFinalizar = 0;
         int personasAtendidas = 0;
         int itemsVendidos = 0;
+        boolean nuevaCajaAgregadaEsteMinuto = false;
+
+        for (int minuto = 1; minuto <= MINUTOS_JORNADA; minuto++) {
+            cleanScreen();
+
+            for (int i = 0; i < numCajas; i++) {
+                cajaOcupada[i] = cajaEstado(itemsClienteCaja[i]);
+            }
+
+            if (llegaCliente()) {
+                cola++;
+                System.out.println("MINUTO " + minuto + " - Llega 1 persona - En Cola: " + cola);
+            } else {
+                System.out.println("MINUTO " + minuto + " - Llegan 0 personas - En Cola: " + cola);
+                minutosSinCola++;
+            }
+
+            if (cola > MAXIMO_PERSONAS_EN_COLA_NUEVA_CAJA && !nuevaCajaAgregadaEsteMinuto) {
+                numCajas++;
+                System.out.println("Nueva caja agregada. Total de cajas: " + numCajas);
+                cola = 0;
+                cajaOcupada = Arrays.copyOf(cajaOcupada, numCajas);
+                itemsClienteCaja = Arrays.copyOf(itemsClienteCaja, numCajas);
+                nuevaCajaAgregadaEsteMinuto = true;
+            }
+
 
     }
 }
