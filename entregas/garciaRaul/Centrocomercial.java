d90c9b7ccc6f182b8c101c8c5a46075a45227ce6
xImRaul <144956120+xImRaul@users.noreply.github.com>
2024-02-12 21:26:18 +0100

    Reto ampliado.java

--- entregas/garciaRaul/Centrocomercial.java
+++ entregas/garciaRaul/Centrocomercial.java
@@ -1,20 +1,30 @@
+import java.util.Arrays;
+
 public class CentroComercial {
     public static void main(String[] args) {
         final int MINUTOS_JORNADA = 720;
-        final int NUM_CAJAS = 4;
+        final int NUM_CAJAS_INICIALES = 4;
+        final int PERSONAS_POR_CAJA = 5;
+        final int MAX_PERSONAS_EN_COLA_PARA_NUEVA_CAJA = 15;
+
         int cola = 0;
-        boolean[] cajaOcupada = new boolean[NUM_CAJAS];
-        int[] itemsClienteCaja = new int[NUM_CAJAS];
+        int numCajas = NUM_CAJAS_INICIALES;
+        boolean[] cajaOcupada = new boolean[NUM_CAJAS_INICIALES];
+        int[] itemsClienteCaja = new int[NUM_CAJAS_INICIALES];
         int minutosSinCola = 0;
         int personasEnColaAlFinalizar = 0;
         int personasAtendidas = 0;
         int itemsVendidos = 0;
 
+        boolean nuevaCajaAgregadaEsteMinuto = false;
+
         for (int minuto = 1; minuto <= MINUTOS_JORNADA; minuto++) {
             cleanScreen();
-            for (int i = 0; i < NUM_CAJAS; i++) {
+
+            for (int i = 0; i < numCajas; i++) {
                 cajaOcupada[i] = cajaEstado(itemsClienteCaja[i]);
             }
+
             if (llegaCliente()) {
                 cola++;
                 System.out.println("MINUTO " + minuto + " - Llega 1 persona - En Cola: " + cola);
@@ -23,7 +33,16 @@ public class CentroComercial {
                 minutosSinCola++;
             }
 
-            for (int i = 0; i < NUM_CAJAS; i++) {
+            if (cola > MAX_PERSONAS_EN_COLA_PARA_NUEVA_CAJA && !nuevaCajaAgregadaEsteMinuto) {
+                numCajas++;
+                System.out.println("Nueva caja agregada. Total de cajas: " + numCajas);
+                cola = 0;
+                cajaOcupada = Arrays.copyOf(cajaOcupada, numCajas);
+                itemsClienteCaja = Arrays.copyOf(itemsClienteCaja, numCajas);
+                nuevaCajaAgregadaEsteMinuto = true;
+            }
+
+            for (int i = 0; i < numCajas; i++) {
                 if (cajaOcupada[i] && itemsClienteCaja[i] > 0) {
                     itemsClienteCaja[i]--;
                     System.out.println("El cliente de la caja " + (i + 1) + " tiene un total de: " + itemsClienteCaja[i]);
@@ -41,7 +60,6 @@ public class CentroComercial {
             }
 
             imprimirEstadoCajas(itemsClienteCaja);
-
             pause(2);
         }
 
@@ -54,7 +72,6 @@ public class CentroComercial {
         System.out.println("Número de items vendidos en el día: " + itemsVendidos);
     }
 
-
     static void imprimirEstadoCajas(int[] itemsClienteCaja) {
         System.out.print("Estado de las cajas: ");
         for (int i = 0; i < itemsClienteCaja.length; i++) {
@@ -62,19 +79,24 @@ public class CentroComercial {
         }
         System.out.println();
     }
+
     static boolean cajaEstado(int items) {
         return items > 0;
     }
+
     static int items() {
         return (int) (Math.random() * 11) + 5;
     }
+
     static boolean llegaCliente() {
         return Math.random() <= 0.6;
     }
+
     static void cleanScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
+
     static void pause(int seconds) {
         try {
             Thread.sleep(1000 * seconds);
@@ -83,4 +105,3 @@ public class CentroComercial {
         }
     }
 }
-
