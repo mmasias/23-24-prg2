cf70415281ca368e7c95b70e8b341a2284710ba9
mariiooo <mario.viadero@alumnos.uneatlantico.es>
2024-02-12 21:25:54 +0100

    Declaracion de variable

---
+++ entregas/viaderoMario/Reto001.java
@@ -0,0 +1,16 @@
+
+public class Reto001 {
+
+  public static void main(String[] args) {
+    int[] box = { 0, 0, 0, 0, 0 };
+    int[] items = { 0, 0, 0, 0, 0 };
+    boolean[] free = { true, true, true, true, true };
+    int queque = 0;
+    final int minutesPerDay = 12 * 60;
+    int nClients = 0;
+    boolean newPerson = false;
+    int timeWithoutQueque = 0;
+    int totalItems = 0;
+
+  }
+}
