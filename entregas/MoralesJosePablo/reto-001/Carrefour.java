8e7ee890890ffb8006d2a9423b98a441a08e88a2
JPMorales04 <jpmorales.epic@gmail.com>
2024-02-12 20:20:31 +0100

    Cola
    Se define la cantidad de clientes que pasan en la cola cada hora

--- entregas/MoralesJosePablo/reto-001/Carrefour.java
+++ entregas/MoralesJosePablo/reto-001/Carrefour.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 
-class CentroComerical_Carrefour{
+class Carrefour {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    final double HORARIO_APERTURA = 9.0;
-    final double HORARIO_CIERRE = 21.0;
-    final double DURACION_ATENCION = 1.0 / 60.0;
-    double atendiendoclientes = HORARIO_APERTURA;
-    boolean trabajando = true;
+        Scanner scanner = new Scanner(System.in);
+        final double HORARIO_APERTURA = 9.0;
+        final double HORARIO_CIERRE = 21.0;
+        final double DURACION_ATENCION = 1.0 / 60.0;
+        double atendiendoclientes = HORARIO_APERTURA;
+        boolean trabajando = true;
 
-    do {
-        atendiendoclientes += DURACION_ATENCION;
-        System.out.println("Hora actual: " + atendiendoclientes);
-        scanner.nextLine();
-        trabajando = atendiendoclientes < HORARIO_CIERRE;
-    } while (trabajando);
+        int cola = 0;
+        final double PROBABILIDAD_LLEGADA_CLIENTES = 0.6;
+
+        do {
+            atendiendoclientes += DURACION_ATENCION;
+            System.out.println("Hora actual: " + atendiendoclientes);
+            trabajando = atendiendoclientes < HORARIO_CIERRE;
+            if (Math.random() < PROBABILIDAD_LLEGADA_CLIENTES) {
+                cola = cola + 1;
+            }
+            System.out.print("Clientes en la cola: " + cola + "\n");
+            scanner.nextLine();
+
+        } while (trabajando);
     }
 }
