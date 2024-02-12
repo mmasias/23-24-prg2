65998ae5eeb8f2f4c460c4e9122effbd9bb6f9e5
eirik.rosete <eirik.rosete@gmail.com>
2024-02-12 13:44:47 +0100

    Implementacion del tiempo actual + Impresion de la fila de los clientes
    Actualmente, imprime la hora y tambien se agrego la posibilidad de ver la cantidad de clientes (con un promedio de 432 clientes en total) a los cuales aun hace falta que sean atendidos por las cajas que vamos a agregar. Asimismo, en proximos commits se vera una limpieza en psvm

--- entregas/roseteEirik/reto-001/carrefour.java
+++ entregas/roseteEirik/reto-001/carrefour.java
@@ -3,11 +3,25 @@ class carrefour{
         
         final int DAY_HOURS = 12;
         final int DAY_MINUTES = DAY_HOURS * 60;
-
+        int costumerQueue = 0;
+        
         for(int minutes = 0; minutes <= DAY_MINUTES; minutes++){
-            
+            currentTime(minutes);
+            boolean costumerSpawnProbability = Math.random() < 0.6;
+            costumerQueue = costumerQueue + (costumerSpawnProbability ? 1 : 0);
+            System.out.println(costumerQueue);
         }
 
+    }
+
+    static void currentTime(int minutes) {
+        int hour = 9 + (minutes / 60);
+        minutes = minutes % 60;
+        String division = ":";
+        if (minutes < 10){
+            division = ":0";
+        }
+        System.out.println(hour + division + minutes);
     }
 
     static void cleanScreen() {
