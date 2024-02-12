37387c73445411bb7c902d41f08919b1e21ed000
nicolas.lopez <nicolas.lopez@alumnos.uneatlantico.es>
2024-02-12 13:10:28 +0100

    Inicializando variables en la main

--- entregas/LopezNicolas/reto_001/CCCF.java
+++ entregas/LopezNicolas/reto_001/CCCF.java
@@ -6,6 +6,14 @@ public class CCCF {
 
     public static void main(String[] args) {
 
+        int [] queue = new int [720];
+        int [] cashiers = new int [4];
+        int customers_total = 0;
+        int items_sold = 0;
+        int queue_size = 0;
+        boolean people_queued = false;
+
+        
     }
 
 }
