03109e532cb7e5d2ab44c57b60d032052d7c5aca
Dreyes2907 <dreyb05@gmail.com>
2024-02-12 15:39:51 +0100

    Tercer Commit
    Seguimiento de la creacion de las condiciones para que a medida que pasa el tiempo Se vayan agregando mas cajas dependiendo de la cantidad de personas en cola

--- entregas/reyesDavid/Reto01/CentroComercial.java
+++ entregas/reyesDavid/Reto01/CentroComercial.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class CentroComercial {
     public static void main(String[] args) {
         final int MINUTOS_JORNADA_TOTAL = 720;
@@ -13,10 +15,10 @@ public class CentroComercial {
         int personasEnColaAlFinalizar = 0;
         int personasAtendidas = 0;
         int itemsVendidos = 0;
-       
+
         boolean nuevaCajaAgregadaEsteMinuto = false;
 
-        for (int minuto = 1; minuto <= MINUTOS_JORNADA; minuto++) {
+        for (int minuto = 1; minuto <= MINUTOS_JORNADA_TOTAL; minuto++) {
             cleanScreen();
 
             for (int i = 0; i < numCajas; i++) {
@@ -30,6 +32,33 @@ public class CentroComercial {
                 System.out.println("MINUTO " + minuto + " - Llegan 0 personas - En Cola: " + cola);
                 minutosSinCola++;
             }
+
+            if (cola > MAX_PERSONAS_EN_COLA_PARA_NUEVA_CAJA && !nuevaCajaAgregadaEsteMinuto) {
+                numCajas++;
+                System.out.println("Nueva caja agregada. Total de cajas: " + numCajas);
+                cola = 0;
+                cajaOcupada = Arrays.copyOf(cajaOcupada, numCajas);
+                itemsClienteCaja = Arrays.copyOf(itemsClienteCaja, numCajas);
+                nuevaCajaAgregadaEsteMinuto = true;
+            }
+
+            for (int i = 0; i < numCajas; i++) {
+                if (cajaOcupada[i] && itemsClienteCaja[i] > 0) {
+                    itemsClienteCaja[i]--;
+                    System.out
+                            .println("El cliente de la caja " + (i + 1) + " tiene un total de: " + itemsClienteCaja[i]);
+                    itemsVendidos++;
+                }
+                if (cola >= 1 && !cajaOcupada[i]) {
+                    System.out.println("Ha pasado a la caja " + (i + 1) + " un cliente");
+                    cola--;
+                    cajaOcupada[i] = true;
+                    itemsClienteCaja[i] = items();
+                    System.out.println("El cliente tiene un total de: " + itemsClienteCaja[i]);
+                    personasAtendidas++;
+                }
+            }
+
         }
     }
 }
