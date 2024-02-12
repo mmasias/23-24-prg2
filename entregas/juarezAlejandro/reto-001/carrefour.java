db4db394a0cbcb10ec2e08d7a3181f3dc3a30ece
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2024-02-12 12:54:19 +0100

    Avance del tiempo
    Debido a que se reinició el codigo desde 0, lo primero será hacer que pase el tiempo de 9 a 21.

--- entregas/juarezAlejandro/reto-001/carrefour.java
+++ entregas/juarezAlejandro/reto-001/carrefour.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 
 class Carrefour {
     public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            scanner.close();
-        }
+        Scanner scanner = new Scanner(System.in);
+        final double HORA_APERTURA = 9.0;
+        final double HORA_CIERRE = 21.0;
+        final double MINUTOS = 1.0 / 60.0;
+        double atendiendo = HORA_APERTURA;
+        boolean trabajando = true;
+
+        do {
+            atendiendo += MINUTOS;
+            System.out.println("Hora actual: " + atendiendo);
+            scanner.nextLine();
+            trabajando = atendiendo < HORA_CIERRE;
+        } while (trabajando);
+        scanner.close();
+    }
 }
