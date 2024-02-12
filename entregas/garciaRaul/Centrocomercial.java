ff66eb03d794810ad4c945d120fcae2f15c9747a
xImRaul <144956120+xImRaul@users.noreply.github.com>
2024-02-12 14:33:42 +0100

    Estructura y variables.java

--- entregas/garciaRaul/Centrocomercial.java
+++ entregas/garciaRaul/Centrocomercial.java
@@ -1,5 +1,12 @@
-class CentroComercial {
+public class CentroComercial {
     public static void main(String[] args) {
 
+        final int MINUTOS_JORNADA = 720;
+        final int NUM_CAJAS = 4;
+
+        int cola = 0;
+
+        boolean[] cajaOcupada = new boolean[NUM_CAJAS];
+        int[] itemsClienteCaja = new int[NUM_CAJAS];
     }
 }
