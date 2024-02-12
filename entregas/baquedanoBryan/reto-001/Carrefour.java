666d1cdaf25320048628f8c9dae99582e4f324ae
enmabry <bryanbaquedano11@gmail.com>
2024-02-12 19:30:37 +0100

    Probabilidad de llegada
    Hice un mètodo que calcula si llega un nuevo cliente a la fila, este retorna un valor que se le asigna a nuestra variable de waitingLineCostumer.

--- entregas/baquedanoBryan/reto-001/Carrefour.java
+++ entregas/baquedanoBryan/reto-001/Carrefour.java
@@ -2,19 +2,30 @@ import java.util.Scanner;
 
 class Carrefour {
     public static void main(String[] args) {
-       // Scanner entrada = new Scanner(System.in);
+        // Scanner entrada = new Scanner(System.in);
 
         final int TOTAL_MINUTES_PER_DAY = 720;
         int minutesWorked = 0;
+        final double PROBABILITY = 0.6;
+        int waitingLineCostumer= 0;
 
+        do {
+            double costumerProbability = Math.random();
+            minutesWorked++;
+            waitingLineCostumer = costumerArrival(PROBABILITY, costumerProbability, waitingLineCostumer);
+            System.out.println(waitingLineCostumer);
+ 
 
+        } while (minutesWorked < TOTAL_MINUTES_PER_DAY);
 
-       do {
-        minutesWorked = minutesWorked++;
-        System.out.println("Comprobaciòn de que el while sirve");
-        
-        
-       } while (minutesWorked<TOTAL_MINUTES_PER_DAY);
+    }
+
+    private static int costumerArrival(double probability, double costumerProbability, int waitingLineCostumer) {
+        if (costumerProbability<probability) {
+            waitingLineCostumer++;
+        }
+
+        return waitingLineCostumer;
 
     }
 }
