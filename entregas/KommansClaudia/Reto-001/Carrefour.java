a1b919bd75e9c8365845c70bd46d843598877f17
claaudiaa <claudia.kommans@alumnos.uneatlantico.es>
2024-02-12 07:57:28 +0100

    1 commit

---
+++ entregas/KommansClaudia/Reto-001/Carrefour.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class RetoCCCF {
+    public static void main(String[] args) {
+
+        int minutosDia=12*60;
+        int  personas=0;
+        int  numeroPaquetes=0;
+        int cola=0;
+        int paquetesCaja1=0;
+        int paquetesCaja2=0;
+        int paquetesCaja3=0;
+        int paquetesCaja4=0;
+        int paquetesCaja5=0;
+        double probabilidadPersona;
+        
