e9c4a15cba8b90e15eb59c43b10fb642edf3ddd7
Dreyes2907 <dreyb05@gmail.com>
2024-02-12 15:22:36 +0100

    Segundo commit ya empece a crear la condiciones para cuando lleguen los clientes al supermercado.

--- entregas/reyesDavid/Reto01/CentroComercial.java
+++ entregas/reyesDavid/Reto01/CentroComercial.java
@@ -1,6 +1,6 @@
 public class CentroComercial {
     public static void main(String[] args) {
-        final int MINUTOS_JORNADA = 720;
+        final int MINUTOS_JORNADA_TOTAL = 720;
         final int NUM_CAJAS_INICIALES = 4;
         final int PERSONAS_POR_CAJA = 5;
         final int MAX_PERSONAS_EN_COLA_PARA_NUEVA_CAJA = 15;
@@ -13,5 +13,23 @@ public class CentroComercial {
         int personasEnColaAlFinalizar = 0;
         int personasAtendidas = 0;
         int itemsVendidos = 0;
+       
+        boolean nuevaCajaAgregadaEsteMinuto = false;
 
-      
+        for (int minuto = 1; minuto <= MINUTOS_JORNADA; minuto++) {
+            cleanScreen();
+
+            for (int i = 0; i < numCajas; i++) {
+                cajaOcupada[i] = cajaEstado(itemsClienteCaja[i]);
+            }
+
+            if (llegaCliente()) {
+                cola++;
+                System.out.println("MINUTO " + minuto + " - Llega 1 persona - En Cola: " + cola);
+            } else {
+                System.out.println("MINUTO " + minuto + " - Llegan 0 personas - En Cola: " + cola);
+                minutosSinCola++;
+            }
+        }
+    }
+}
