111f864be1255f10b00d074e2069524aefee4b17
enmabry <bryanbaquedano11@gmail.com>
2024-02-12 19:58:06 +0100

    Asignar clientes a caja vacìa
    Me costò encontrar una manera para poder asignarle el valor a cada caja en un arreglo, pero al final lo conseguì con el for y dentro de este for un if. Màs abajo agreguè unos Sout temporales que me dicen datos que quiero saber.

--- entregas/baquedanoBryan/reto-001/Carrefour.java
+++ entregas/baquedanoBryan/reto-001/Carrefour.java
@@ -7,25 +7,35 @@ class Carrefour {
         final int TOTAL_MINUTES_PER_DAY = 720;
         int minutesWorked = 0;
         final double PROBABILITY = 0.6;
-        int waitingLineCostumer= 0;
+        int waitingLineCostumer = 0;
+        int[] waitingLinePerCashier = { 0, 0, 0, 0 };
 
         do {
             double costumerProbability = Math.random();
             minutesWorked++;
             waitingLineCostumer = costumerArrival(PROBABILITY, costumerProbability, waitingLineCostumer);
-            System.out.println(waitingLineCostumer);
- 
+
+            for (int i = 0; i < waitingLinePerCashier.length && waitingLineCostumer > 0; i++) {
+                if (waitingLinePerCashier[i] == 0) {
+                    waitingLinePerCashier[i]++;
+                    waitingLineCostumer--;
+                }
+            }
+
+            System.out.println("Minuto " + minutesWorked);
+            for (int i = 0; i < waitingLinePerCashier.length; i++) {
+                System.out.println("Caja " + (i + 1) + ": " + waitingLinePerCashier[i] + " cliente(s)");
+            }
+            System.out.println("Clientes en espera: " + waitingLineCostumer);
 
         } while (minutesWorked < TOTAL_MINUTES_PER_DAY);
 
     }
 
     private static int costumerArrival(double probability, double costumerProbability, int waitingLineCostumer) {
-        if (costumerProbability<probability) {
+        if (costumerProbability < probability) {
             waitingLineCostumer++;
         }
-
         return waitingLineCostumer;
-
     }
 }
