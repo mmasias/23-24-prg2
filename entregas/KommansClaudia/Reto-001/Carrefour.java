a99a7b332800c1565ec057323decbe4dadcecf40
claaudiaa <claudia.kommans@alumnos.uneatlantico.es>
2024-02-12 08:10:28 +0100

    2commit

--- entregas/KommansClaudia/Reto-001/Carrefour.java
+++ entregas/KommansClaudia/Reto-001/Carrefour.java
@@ -15,4 +15,36 @@ public class RetoCCCF {
         int paquetesCaja4=0;
         int paquetesCaja5=0;
         double probabilidadPersona;
-        
+
+        for (int i=1; i <= minutosDia; i++) {
+            probabilidadPersona = Math.random();
+            numeroPaquetes = 0;
+            if ( probabilidadPersona <= 0.4) {
+                personas = 1;
+            }
+            else {
+                personas = 0;
+            }
+            if (personas == 1) {
+                numeroPaquetes = (int)(Math.random() * 11 + 5);
+                cola++;
+            }
+            else if (cola != 0) {
+                numeroPaquetes = (int)(Math.random() * 11 + 5);
+            }
+
+            if (paquetesCaja1 != 0){
+                paquetesCaja1--;
+            }
+            if (paquetesCaja2 != 0){
+                paquetesCaja2--;
+            }
+            if (paquetesCaja3 != 0){
+                paquetesCaja3--;
+            }
+            if (paquetesCaja4 != 0){
+                paquetesCaja4--;
+            }
+            if (paquetesCaja5 != 0){
+                paquetesCaja5--;
+            }
