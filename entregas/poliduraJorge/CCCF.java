aba0d2de84d028e88d1c5b8ecd0ccce78d1da866
JorgePolidura123 <144842516+JorgePolidura123@users.noreply.github.com>
2024-02-12 18:01:28 +0100

    Update CCCF.java
    Creo la base minima del programa

--- entregas/poliduraJorge/CCCF.java
+++ entregas/poliduraJorge/CCCF.java
@@ -1,1 +1,22 @@
+public class CCCF {
+    public static void main(String[] args) {
+
+        int initialTime=9;
+        int finalTime=21;
+        int hours=finalTime-initialTime;
+        int totalMinutes=hours*60;
+        int minutes=1;
+
+        do{
+
+            
+            
+            
+            
+            minutes++;
+        }while (minutes<=totalMinutes);
+
+
+    }
+}
 
