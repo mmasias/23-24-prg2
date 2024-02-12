ceb651415583d648465e8798c83dc43d70bcec18
eirik.rosete <eirik.rosete@gmail.com>
2024-02-12 13:04:10 +0100

    cleanScreen + pause
    Preparando codigo para su desarrollo

--- entregas/roseteEirik/reto-001/carrefour.java
+++ entregas/roseteEirik/reto-001/carrefour.java
@@ -2,4 +2,16 @@ class carrefour{
     public static void main(String[] args) {
         
     }
+
+    static void cleanScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+    
+    static void pause(double seconds) {
+        try {
+            Thread.sleep((int) (1000 * seconds));
+        } catch (InterruptedException e) {
+        }
+    }
 }
