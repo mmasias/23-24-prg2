1af8425635c5d2e0d814d94a2a8084241c6f01ae
pareyor <pablo.reyortiz@alumnos.uneatlantico.es>
2024-02-12 21:41:29 +0100

    definicion de variables (incluyendo reto extendido)

--- entregas/reyPablo/reto-001/CarrefourMain.java
+++ entregas/reyPablo/reto-001/CarrefourMain.java
@@ -1,9 +1,18 @@
 import java.util.Random;
 
 public class CarrefourMain {
-    
-    public static void main(String[] args) {
 
-        
+    public static void main(String[] args) {
+        int[] caja = { 0, 0, 0, 0, 0 };
+        int[] nItems = { 0, 0, 0, 0, 0 };
+        boolean[] libre = { true, true, true, true, true };
+        int cola = 0;
+        final int minutosEnUnDia = 12 * 60;
+        int numeroDeClientes = 0;
+        boolean llegaNuevo = false;
+        int minutosSinCola = 0;
+        int totalItems = 0;
+        boolean activa = false;
+
     }
-    }
+}
