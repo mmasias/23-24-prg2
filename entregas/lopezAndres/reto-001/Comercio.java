bc39f2d9b5191035ca90ea15fe7787a9a7ed7ab0
andreslb9 <144810780+andreslb9@users.noreply.github.com>
2024-02-12 22:52:31 +0100

    Primer Commit

---
+++ entregas/lopezAndres/reto-001/Comercio.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Comercio {
+    public static void main(String[] args) {
+        final int MINUTOS_JORNADA = 720;
+        final int CAJAS_INICIALES = 4;
+        final int PERSONAS_POR_CAJA = 5;
+        final int MAXIMO_PERSONAS_EN_COLA_NUEVA_CAJA = 15;
+
+        int cola = 0;
+        int numCajas = CAJAS_INICIALES;
+        boolean[] cajaOcupada = new boolean[CAJAS_INICIALES];
+        int[] itemsClienteCaja = new int[CAJAS_INICIALES];
+        int minutosSinCola = 0;
+        int personasEnColaAlFinalizar = 0;
+        int personasAtendidas = 0;
+        int itemsVendidos = 0;
+
+    }
+}
