895e5b0576763f9f6582420ed5d5afd18e964687
ManuelaGrzoni <manuela.grizoni@alumnos.uneatlantico.es>
2024-02-13 00:27:26 +0100

    Commit3

--- entregas/grizoniManuela/reto001.java
+++ entregas/grizoniManuela/reto001.java
@@ -24,6 +24,7 @@ class Caja {
 
 public class reto001 {
     public static void main(String[] args){
+
         int tiempo = 720;
         int packsPorMinuto = 1;
         int probabilidaDeLLegadaCliente= 40;
@@ -33,16 +34,48 @@ public class reto001 {
         Caja[] cajas = new Caja[cantidadDeCajas];
 
         for (int i=0; i< cantidadDeCajas; i++){
+
             cajas[i] = new Caja ();
         }
 
         Random random = new Random();
 
         System.out.println("MINUTO 1 - Llega 1 persona - En cola:0");
-       
-
         
 
+        for (int minuto = 2; minuto <= tiempo; minuto++){
+
+            if (random.nextInt(100) < probabilidaDeLLegadaCliente){
+
+                int packsCliente = random.nextInt(11)+5;
+                Cliente nuevoCliente = new Cliente(packsCliente);
+                cola.add(nuevoCliente);
+                System.out.println("MINUTO" + minuto + " - Llega 1 persona  - En cola: " + cola.size());
+
+            }else{
+                System.out.println("MINUTO" + minuto + " - No llega nadie  - En cola: " + cola.size());
+            }
+
+            for (int i = 0; i < cantidadDeCajas; i++){
+
+                if (cajas[i].ocupada){
+                    cajas[i].ocupada = false;
+
+                }else if (!cola.isEmpty()){
+
+                    Cliente clienteActual = cola.poll();
+                    int tiempoAtencionTotal = clienteActual.packs * packsPorMinuto;
+
+                    for (int j = 0; j < tiempoAtencionTotal; j++){
+                    }
+
+                    cajas[i].ocupada = true;
+                }
+            }
+
+           
+
+        }
 
     }
     
