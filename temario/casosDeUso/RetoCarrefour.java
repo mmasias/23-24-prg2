6a20b28697b41285cd72c199e16f4fc07dfc4146
iansaucedo <s.ians2212@gmail.com>
2024-02-12 18:00:40 +0100

    Commit inicial, donde se plantea el tiempo, ademas de las variables que corresponderan a cada caja de manera tal que se pueda iniciar a desarrolar el problema

---
+++ temario/casosDeUso/RetoCarrefour.java
@@ -0,0 +1,16 @@
+public class RetoCarrefour {
+    public static void main(String[] args) {
+        final int MINUTE_IN_HOUR = 60 ;
+        int hoursWorking = 9;
+        int timeWorking= 1;
+        int MaxTimeForWork= hoursWorking*MINUTE_IN_HOUR;
+        int cashier1 = 0, cashier2 = 0, cashier3= 0, cashier4=0;
+        boolean isWorking = true;
+        int peopleInLine = 0;
+        
+     do {
+        
+     } while (isWorking == true);
+        
+    }
+}
