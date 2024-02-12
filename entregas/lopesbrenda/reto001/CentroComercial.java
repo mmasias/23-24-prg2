a7fd4f5fdd3076172d483131ea64eef2ff2dd328
brendalopes <brenda14lopess@gmail.com>
2024-02-13 00:00:21 +0100

    Commit 2

--- entregas/lopesbrenda/reto001/CentroComercial.java
+++ entregas/lopesbrenda/reto001/CentroComercial.java
@@ -7,9 +7,9 @@ public class CentroComercial {
         final int HORAS_DE_TRABAJO = 12;
         final double PROBABILIDAD_LLEGADA_CLIENTE = 0.40;
 
-
         Random random = new Random();
         int clientesEnCola = 0;
+        int[] cajas=new int[4];
 
         for (int hora = 0; hora < HORAS_DE_TRABAJO; hora++) {
             System.out.println("Hora " + hora + ":");
@@ -18,8 +18,22 @@ public class CentroComercial {
                 if(random.nextDouble()< PROBABILIDAD_LLEGADA_CLIENTE){
                     clientesEnCola++;
                 }
+                for(int i=0; i<4; i++){
+                    if (clientesEnCola > 0) {
+                        int packs = 5 + random.nextInt(15 - 5 + 1);
+                        cajas[i] = packs * 1;
+                        clientesEnCola--;
+                    }
+                }
             }
 
+            for (int i = 0; i < 4; i++) {
+                if (cajas[i] > 0) {
+                    System.out.println("Caja " + (i + 1) + ": atendiendo (" + cajas[i] + " packs restantes)");
+                } else {
+                    System.out.println("Caja " + (i + 1) + ": libre");
+                }
+            }
             System.out.println("Personas en cola: " + clientesEnCola);
         }
     }
