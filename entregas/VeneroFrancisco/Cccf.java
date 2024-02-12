4c32cff24f7593e2333eb9f15cdc2562c346f98b
franvenero <franvenero8@gmail.com>
2024-02-12 22:13:45 +0100

    Segundo Commit

--- entregas/VeneroFrancisco/Cccf.java
+++ entregas/VeneroFrancisco/Cccf.java
@@ -1,5 +1,19 @@
-class Cccf {
-    public static void main(String[] args) {
+import java.util.Random;
+
+    class Cccf {
+        public static void main(String[] args) {
+            int[] caja ={0,0,0,0,0};
+            int[] nItems ={0,0,0,0,0};
+            boolean[] libre ={true,true,true,true,true};
+            int cola = 0;
+            final int minutosEnUnDia = 12 * 60;
+            int numeroDeClientes = 0;
+            boolean llegaNuevo = false;
+            int minutosSinCola = 0;
+            int totalItems = 0;
+            int caja5 = 0, nItems5 = 0;
+            boolean libre5 = true, activa = false;
+        }
+
 
     }
-}
