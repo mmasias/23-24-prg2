5706ae8defa5e3203e428f609c39a467d7b54253
SdeCos <saul.decossanchez@alumnos.uneatlantico.es>
2024-02-12 15:23:41 +0100

    Reto extendido abrir y cerrar cajas

--- entregas/decosSaul/reto-001/ShoppingCentre.java
+++ entregas/decosSaul/reto-001/ShoppingCentre.java
@@ -15,13 +15,24 @@ public class ShoppingCentre {
         int soldItems = 0;
         
         int[] register = new int[4];
+        int[] registerStatus = new int[4];
         
         final int NEW_CUSTOMER_CHANCE = 60;
         int queue = 0;
         int emptyQueueTime = 0;
         int dailyClients = 0;
 
-        do{ 
+        int modifiedRegister = 0;
+
+        do{
+
+                System.out.println("What cash register do you want to change status? (1 to 4), by default all cash register are open");
+                modifiedRegister = scanner.nextInt() - 1;
+                
+                if (modifiedRegister != -1){
+                    registerStatus[modifiedRegister] += 1;
+                }
+
                 minutesPassed += 1;
             
                 if (minutesPassed % 3 == 0){
@@ -41,17 +52,19 @@ public class ShoppingCentre {
                 emptyQueueTime += queue == 0 ? 1 : 0;
 
                 for (int checking = 0; checking < register.length; checking ++){
-                    if (register[checking] > 0){
-                        register[checking] -= 1;
-                        soldItems += 1;
-                    } else if (queue >= 1){
-                        queue -= 1;
-                        dailyClients += 1;
-                        register[checking] = randomNumber(MINIMUM_ITEMS, MAXIMUM_ITEMS);
+                    if (registerStatus[checking] % 2 == 0){
+                        if (register[checking] > 0){
+                            register[checking] -= 1;
+                            soldItems += 1;
+                        } else if (queue >= 1){
+                            queue -= 1;
+                            dailyClients += 1;
+                            register[checking] = randomNumber(MINIMUM_ITEMS, MAXIMUM_ITEMS);
+                        }
                     }
                 }
 
-            System.out.println("Register1:[" + register[0] + "] | Register2:[" + register[1] + "] | Register3:[" + register[2] + "] | Register4:[" + register[3] + "]");
+            System.out.println("CashRegister1:[" + register[0] + "] | CashRegister2:[" + register[1] + "] | CashRegister3:[" + register[2] + "] | CashRegister4:[" + register[3] + "]");
             System.out.println();
                 
             scanner.nextLine();
