2b927d606b3b0d339087a1830b34766a20ad1eca
eirik.rosete <eirik.rosete@gmail.com>
2024-02-13 23:15:19 +0100

    Se agregaron las estadisticas
    Se han implementado las estadisticas que conforman el resumen del dia [los minutos donde nadie estaba en la cola, las personas que se quedaron en la fila (que no 'sirve' porque el programa deja de funcionar hasta que se hayan ido todos los clientes), todos los clientes atendidos y todos los articulos vendidos]. Asimismo, se agrego pause (1 sec) y cleanScreen al programa

--- entregas/roseteEirik/reto-001/carrefour.java
+++ entregas/roseteEirik/reto-001/carrefour.java
@@ -7,27 +7,75 @@ class Carrefour {
         int customerQueue = 0;
         int minutes = 0;
         int[] quantityOfItems = { 0, 0, 0, 0 };
+        int[] totalItems = { 0, 0, 0, 0 };
         boolean workingHours = true;
+
+        int zeroQueueStat = 0;
+        int customersAttended = 0;
 
         do {
             minutes++;
-            workingHours = minutes < DAY_MINUTES;
             currentTime(minutes);
+            workingHours = minutes < DAY_MINUTES;
             customerQueue = customerQueue + addCustomer(workingHours);
-            customerQueue = customerQueue - cashierWorking(quantityOfItems, customerQueue);
-            System.out.println(customerQueue);
+            message(customerQueue, quantityOfItems);
+            customersAttended = customersAttended + customerQueue;
+            customerQueue = customerQueue - cashierWorking(quantityOfItems, customerQueue, totalItems);
+            customersAttended = customersAttended - customerQueue;
+            zeroQueueStat = zeroQueueStat + emptyQueue(customerQueue);
+            pause(1);
+            cleanScreen();
 
-        } while (allCustomersLeft(quantityOfItems, workingHours));
+        } while (allCustomersLeft(quantityOfItems, workingHours, customerQueue));
+
+        finalStats(zeroQueueStat, customerQueue, customersAttended, totalItems);
     }
 
-    static boolean allCustomersLeft(int[] quantityOfItems, boolean workingHours) {
+    static int emptyQueue(int customerQueue){
+        if(customerQueue == 0){
+            return 1;
+        }
+        return 0;
+    }
+
+    static void finalStats(int zeroQueueStat, int customerQueue, int customersAttended, int[] totalItems){
+        System.out.println("RESUMEN");
+        System.out.println("=".repeat(50));
+        System.out.println("Minutos con cola en cero: " + zeroQueueStat);
+        System.out.println("Personas en la cola al cierre: " + customerQueue);
+        System.out.println("Personas atendidas en el dia: " + customersAttended);
+        System.out.println("Articulos vendidos en el dia: " + addItems(totalItems));
+        System.out.println("=".repeat(50));
+    }
+
+    static int addItems(int[] totalItems){
+        int total = 0;
+        for (int cashierLine = 0; cashierLine < totalItems.length; cashierLine++) {
+            total = total + totalItems[cashierLine];
+        }
+        return total;
+    }
+
+    static void message(int customerQueue, int[] quantityOfItems) {
+        int numberOfItems = 0;
+        System.out.println(" - Fila: " + customerQueue);
+        for (int cashierLine = 0; cashierLine < quantityOfItems.length; cashierLine++) {
+            numberOfItems = quantityOfItems[cashierLine] == 0 ? 0 : quantityOfItems[cashierLine] - 1;
+            System.out.print("Caja" + (cashierLine + 1) + ":[" + (numberOfItems) + "] | ");
+        }
+        System.out.println();
+    }
+
+    static boolean allCustomersLeft(int[] quantityOfItems, boolean workingHours, int customerQueue) {
         boolean cashiersWorking = true;
+        boolean customersStillInShop = true;
         int totalItems = 0;
         for (int cashierLine = 0; cashierLine < quantityOfItems.length; cashierLine++) {
             totalItems = totalItems + quantityOfItems[cashierLine];
         }
         cashiersWorking = totalItems != 0 ? true : false;
-        if (!cashiersWorking && !workingHours) {
+        customersStillInShop = customerQueue > 0 ? true : false;
+        if (!cashiersWorking && !workingHours && !customersStillInShop) {
             return false;
         }
         return true;
@@ -40,48 +88,45 @@ class Carrefour {
         if (minutes < 10) {
             division = ":0";
         }
-        System.out.println("Hora: " + hour + division + minutes);
+        System.out.print("Hora: " + hour + division + minutes);
     }
 
     static int addCustomer(boolean workingHours) {
         final double customerSpawnProbability = 0.6;
         if (Math.random() < customerSpawnProbability && workingHours) {
+            System.out.print(" - Llega una persona");
             return 1;
         }
         return 0;
     }
 
-    static int cashierWorking(int[] quantityOfItems, int customerQueue) {
+    static int cashierWorking(int[] quantityOfItems, int customerQueue, int[] totalItems) {
         final int MIN_ITEMS = 5;
         final int MAX_ITEMS = 15;
         int numberOfCustomersToCheckout = 0;
+        System.out.println();
 
         for (int cashierLine = 0; cashierLine < quantityOfItems.length; cashierLine++) {
             if (quantityOfItems[cashierLine] == 0 && customerQueue > 0) {
                 quantityOfItems[cashierLine] = (int) (Math.random() * (MAX_ITEMS - MIN_ITEMS) + MIN_ITEMS);
+                totalItems[cashierLine] = totalItems[cashierLine] + quantityOfItems[cashierLine];
                 customerQueue--;
                 numberOfCustomersToCheckout++;
             }
             if (quantityOfItems[cashierLine] != 0) {
                 quantityOfItems[cashierLine]--;
             }
-            printASCII(quantityOfItems[cashierLine]);
+            printASCII(quantityOfItems[cashierLine], customerQueue);
         }
-
-        reduceCustomer(numberOfCustomersToCheckout);
         return numberOfCustomersToCheckout;
     }
 
-    static int reduceCustomer(int reduce) {
-        return reduce;
-    }
-
-    static void printASCII(int quantityOfItems) {
+    static void printASCII(int quantityOfItems, int customerQueue) {
         final int EMPLOYEE_RESTING = 0;
         final int EMPLOYEE_WORKING = 1;
         String[] draw = { "O ", "O:" };
-        System.out.println((quantityOfItems == 0 ? draw[EMPLOYEE_RESTING] : draw[EMPLOYEE_WORKING])
-                + "[#]".repeat(quantityOfItems));
+        System.out.println((quantityOfItems == 0 ? draw[EMPLOYEE_RESTING] : draw[EMPLOYEE_WORKING]) + "[#]".repeat(quantityOfItems));
+
     }
 
     static void cleanScreen() {
