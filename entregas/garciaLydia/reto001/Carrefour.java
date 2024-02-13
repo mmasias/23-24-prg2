29945a8310ce01fb45709a1fb5b666c9ae08c2aa
lydiaa-gr <lydia.garcia@alumnos.uneatlantico.es>
2024-02-13 23:09:26 +0100

    mostrar resumen

--- entregas/garciaLydia/reto001/Carrefour.java
+++ entregas/garciaLydia/reto001/Carrefour.java
@@ -12,15 +12,16 @@ public class Carrefour {
         int itemsVendidos = 0;
 
         for (int minuto = 1; minuto <= 720; minuto++) {
-            // Llegada de personas
+
             if (rand.nextDouble() < 0.6) {
                 cola++;
                 System.out.println("MINUTO " + minuto + " - Llega 1 persona - En Cola: " + cola);
             } else {
                 System.out.println("MINUTO " + minuto + " - No llega nadie  - En Cola: " + cola);
+
             }
 
-            // Atención en cajas
+
             for (int i = 0; i < cajas.length; i++) {
                 if (cajas[i] > 0) {
                     cajas[i]--;
@@ -31,17 +32,26 @@ public class Carrefour {
                     personasAtendidas++;
                 }
             }
-            
+
+
             if (cola == 0) {
                 minutosSinCola++;
             }
 
-            // Mostrar estado de cajas
+
             System.out.print("Caja1:[" + cajas[0] + "] | Caja2:[" + cajas[1] + "] | Caja3:[" + cajas[2] + "] | Caja4:[" + cajas[3] + "] | ");
             System.out.println();
             System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
         }
 
+
+        System.out.println("RESUMEN");
+        System.out.println("============================================================");
+        System.out.println("Minutos con cola en cero \t  : " + minutosSinCola);
+        System.out.println("Personas en la cola al cierre : " + cola);
+        System.out.println("Personas atendidas en el dia \t: " + personasAtendidas);
+        System.out.println("Articulos vendidos en el dia \t: " + itemsVendidos);
+        System.out.println("============================================================");
     }
 }
 
