82f7441b72b3ea24b10cb3e4bd05cce8bc48eda9
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2024-02-13 11:17:30 +0100

    In english please
    Se cambió todo el código al idioma inglés a manera de intentar aprender más.

--- entregas/juarezAlejandro/reto-001/Carrefour.java
+++ entregas/juarezAlejandro/reto-001/Carrefour.java
@@ -3,131 +3,131 @@ import java.util.Scanner;
 class Carrefour {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        final double HORA_APERTURA = 9.0;
-        final double HORA_CIERRE = 21.0;
-        final double MINUTOS = 1.0 / 60.0;
-        double atendiendo = HORA_APERTURA;
-        boolean trabajando = true;
-        int minutosSinFila = 0;
-        int totalClientesAtendidos = 0;
-        int totalItemsVendidos = 0;
+        final double OPENING_HOUR = 9.0;
+        final double CLOSING_TIME = 21.0;
+        final double MINUTES = 1.0 / 60.0;
+        double attending = OPENING_HOUR;
+        boolean working = true;
+        int minutesWithoutLine = 0;
+        int totalClientsServed = 0;
+        int totalItemsSold = 0;
 
-        int fila = 0;
-        final double PROBABILIDAD_LLEGADA_CLIENTE = 0.6;
-        final int CANTIDAD_CAJEROS = 4;
-        int [] caja = new int [CANTIDAD_CAJEROS];
-        final int ITEMS_MAXIMO_CLIENTE = 15;
-        final int ITEMS_MINIMO_CLIENTE = 5;
-        boolean clienteCajaExtra = false;
-        int itemsCajaExtra = 0;
+        int row = 0;
+        final double CUSTOMER_ARRIVAL_PROBABILITY = 0.6;
+        final int AMOUNT_ATMS = 4;
+        int [] box = new int [AMOUNT_ATMS];
+        final int MAXIMUM_CUSTOMER_ITEMS = 15;
+        final int MINIMUM_CUSTOMER_ITEMS = 5;
+        boolean customerExtraBox = false;
+        int extraBoxItems = 0;
         
 
-        boolean bienvenida = false;
+        boolean welcome = false;
 
         do {
-            if (atendiendo == HORA_APERTURA && !bienvenida) {
-                imprimeBienvenida();
-                bienvenida = true;
+            if (attending == OPENING_HOUR && !welcome) {
+                printWelcome();
+                welcome = true;
                 System.out.println("Presione enter para iniciar");
                 scanner.nextLine();
                 cleanScreen();
             }
 
             cleanScreen();
-            atendiendo += MINUTOS;
-            int horas = (int) atendiendo;
-            int minutos = (int) ((atendiendo - horas) * 60);
+            attending += MINUTES;
+            int hours = (int) attending;
+            int minutes = (int) ((attending - hours) * 60);
             
-            System.out.println("Hora actual: " + horas + ":" + minutos);
-            imprimeLinea();
-            trabajando = atendiendo < HORA_CIERRE;
-            if (fila == 0){
-                minutosSinFila++;
+            System.out.println("Hora actual: " + hours + ":" + minutes);
+            printLine();
+            working = attending < CLOSING_TIME;
+            if (row == 0){
+                minutesWithoutLine++;
             }
-            if (Math.random() < PROBABILIDAD_LLEGADA_CLIENTE) {
-                fila = fila + 1;
+            if (Math.random() < CUSTOMER_ARRIVAL_PROBABILITY) {
+                row = row + 1;
             }
 
-            if (clienteCajaExtra){
-                if (itemsCajaExtra > 0) {
-                    itemsCajaExtra--;
-                    if (itemsCajaExtra == 0){
+            if (customerExtraBox){
+                if (extraBoxItems > 0) {
+                    extraBoxItems--;
+                    if (extraBoxItems == 0){
                         System.out.println("La caja extra está vacía.");
-                        clienteCajaExtra = false;
+                        customerExtraBox = false;
                     } else {
                         System.out.print("Un cliente se dirige a la caja extra con ");
-                        for (int dibujoItemsExtra = 0; dibujoItemsExtra < itemsCajaExtra; dibujoItemsExtra++){
+                        for (int drawingExtraItems = 0; drawingExtraItems < extraBoxItems; drawingExtraItems++){
                             System.out.print("[#]");
                         }
-                        System.out.println(" items. Lo que es igual a [" + itemsCajaExtra + "] items.");
+                        System.out.println(" items. Lo que es igual a [" + extraBoxItems + "] items.");
                     }
                 }
             }
 
-            if (fila > 15 && !clienteCajaExtra) {
-                for (int cajaExtra = 0; cajaExtra < CANTIDAD_CAJEROS; cajaExtra++) {
-                    if (caja[cajaExtra] <= 0 && !clienteCajaExtra) {
-                        caja[cajaExtra] = (int) (Math.random() * (ITEMS_MAXIMO_CLIENTE - ITEMS_MINIMO_CLIENTE))  + ITEMS_MINIMO_CLIENTE;
-                        fila--;
-                        totalClientesAtendidos++;
-                        totalItemsVendidos += caja[cajaExtra];
-                        clienteCajaExtra = true;
-                        itemsCajaExtra = caja[cajaExtra];
+            if (row > 15 && !customerExtraBox) {
+                for (int extraBox = 0; extraBox < AMOUNT_ATMS; extraBox++) {
+                    if (box[extraBox] <= 0 && !customerExtraBox) {
+                        box[extraBox] = (int) (Math.random() * (MAXIMUM_CUSTOMER_ITEMS - MINIMUM_CUSTOMER_ITEMS))  + MINIMUM_CUSTOMER_ITEMS;
+                        row--;
+                        totalClientsServed++;
+                        totalItemsSold += box[extraBox];
+                        customerExtraBox = true;
+                        extraBoxItems = box[extraBox];
                     }
                 }
             }
 
-            for (int atender = 0; atender < CANTIDAD_CAJEROS; atender++){
-                if (caja[atender] <= 0 && fila > 0){
-                    fila --;
-                    totalClientesAtendidos++;
-                    int itemsVenta = (int) (Math.random() * (ITEMS_MAXIMO_CLIENTE - ITEMS_MINIMO_CLIENTE)) + ITEMS_MINIMO_CLIENTE;
-                    totalItemsVendidos += itemsVenta;
-                    caja[atender] = itemsVenta;
-                } else if (caja[atender] > 0){
-                    caja[atender]--;
+            for (int attend = 0; attend < AMOUNT_ATMS; attend++){
+                if (box[attend] <= 0 && row > 0){
+                    row --;
+                    totalClientsServed++;
+                    int itemsSale = (int) (Math.random() * (MAXIMUM_CUSTOMER_ITEMS - MINIMUM_CUSTOMER_ITEMS)) + MINIMUM_CUSTOMER_ITEMS;
+                    totalItemsSold += itemsSale;
+                    box[attend] = itemsSale;
+                } else if (box[attend] > 0){
+                    box[attend]--;
                 }
 
-                if (caja[atender] > 0){
-                    System.out.print("Caja " + (atender + 1) + " atiende al cliente y le quedan ");
-                    for (int dibujoItems = 0; dibujoItems < caja[atender]; dibujoItems++){
+                if (box[attend] > 0){
+                    System.out.print("Caja " + (attend + 1) + " atiende al cliente y le quedan ");
+                    for (int drawingItems = 0; drawingItems < box[attend]; drawingItems++){
                         System.out.print("[#]");
                     }
-                    System.out.println(" items. Lo que es igual a que le quedan [" + caja[atender] + "] items.");
+                    System.out.println(" items. Lo que es igual a que le quedan [" + box[attend] + "] items.");
                 } else {
-                    System.out.println("Caja " + (atender + 1) + " se encuentra vacia.");
+                    System.out.println("Caja " + (attend + 1) + " se encuentra vacia.");
                 }
             }
-            imprimeLinea();
-            System.out.println("Cantidad de clientes en fila: " + fila);
-            imprimePersona(fila);
-            imprimeLinea();
+            printLine();
+            System.out.println("Cantidad de clientes en fila: " + row);
+            printPerson(row);
+            printLine();
             scanner.nextLine();
-        } while (trabajando);
+        } while (working);
 
         System.out.println("RESUMEN");
-        imprimeLineaResumen();
-        System.out.println("Cantidad de minutos en los que no hubo nadie en la fila: " + minutosSinFila);
-        System.out.println("Cantidad de clientes que permanecieron en fila a la hora de cierre: " + fila);
-        System.out.println("Al finalizar el día se atendieron " + totalClientesAtendidos + " clientes.");
-        System.out.println("En total se vendieron " + totalItemsVendidos + " items.");
-        imprimeLineaResumen();
+        printSummaryLine();
+        System.out.println("Cantidad de minutos en los que no hubo nadie en la fila: " + minutesWithoutLine);
+        System.out.println("Cantidad de clientes que permanecieron en fila a la hora de cierre: " + row);
+        System.out.println("Al finalizar el día se atendieron " + totalClientsServed + " clientes.");
+        System.out.println("En total se vendieron " + totalItemsSold + " items.");
+        printSummaryLine();
         scanner.close();
     }
 
-    static void imprimePersona(int fila) {
-        int numero = fila / 10 + 1;
-        int repeticion = fila % 10;
+    static void printPerson(int row) {
+        int number = row / 10 + 1;
+        int repetition = row % 10;
         do {
-            numero--;
-            System.out.println("   O ".repeat(fila = numero != 0 ? 10 : repeticion));
-            System.out.println("  /|\\".repeat(fila = numero != 0 ? 10 : repeticion));
-            System.out.println("  / \\".repeat(fila = numero != 0 ? 10 : repeticion));
+            number--;
+            System.out.println("   O ".repeat(row = number != 0 ? 10 : repetition));
+            System.out.println("  /|\\".repeat(row = number != 0 ? 10 : repetition));
+            System.out.println("  / \\".repeat(row = number != 0 ? 10 : repetition));
             System.out.println();
-        } while (numero > 0);
+        } while (number > 0);
     }
 
-    static void imprimeBienvenida() {
+    static void printWelcome() {
         System.out.println("  ______      ___      .______      .______       _______  _______   ______    __    __  .______      ");
         System.out.println(" /      |    /   \\     |   _  \\     |   _  \\     |   ____||   ____| /  __  \\  |  |  |  | |   _  \\     ");
         System.out.println("|  ,----'   /  ^  \\    |  |_)  |    |  |_)  |    |  |__   |  |__   |  |  |  | |  |  |  | |  |_)  |    ");
@@ -135,10 +135,10 @@ class Carrefour {
         System.out.println("|  `----. /  _____  \\  |  |\\  \\----.|  |\\  \\----.|  |____ |  |     |  `--'  | |  `--'  | |  |\\  \\----.");
         System.out.println(" \\______|/__/     \\__\\ | _| `._____|| _| `._____||_______||__|      \\______/   \\______/  | _| `._____|");
     }
-    static void imprimeLineaResumen() {
+    static void printSummaryLine() {
         System.out.println("===========".repeat(7));
     }
-    static void imprimeLinea(){
+    static void printLine(){
         System.out.println("---------+".repeat(5));
     }
 
