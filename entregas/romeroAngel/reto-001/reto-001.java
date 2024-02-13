d7de8af986dbd61ff90cf249a9ab3678e20f4454
Romero <rxmerx504@gmail.com>
2024-02-13 13:01:21 +0100

    Implementacion de los métodos estáticos que se encargaran de: La Llegada de los Clientes, Llevar Clientes a las Colas, siempre y cuando estén vacias, y Mostrar Datos(tiempo, clientes en cola, etc.)

--- entregas/romeroAngel/reto-001/reto-001.java
+++ entregas/romeroAngel/reto-001/reto-001.java
@@ -3,15 +3,59 @@ import java.util.Scanner;
 class RetoCarrefour {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
         final int CLOSING_TIME = 720;
+        final double CLIENT_ARRIVAL_PROBABILITY = 0.4;
+
         int currentMinute = 0;
+        int queue = 0;
+        boolean working;
 
-        boolean working = currentMinute<CLOSING_TIME;
+        int itemsMaximum = 15;
+        int itemsMinimum = 5;
 
-        do{
+        int[] cashiers = { 0, 0, 0, 0 };
 
-        }while(working);
+        do {
+            queue = clientArrives(queue, CLIENT_ARRIVAL_PROBABILITY);
+            cashiers = sendClientToCashier(cashiers, queue, itemsMaximum, itemsMinimum);
+            currentMinute++;
+            showData(queue, cashiers, currentMinute);
+
+            scanner.nextLine();
+            
+            working = currentMinute < CLOSING_TIME;
+        } while (working);
+
+    }
+
+    private static void showData(int queue, int[] cashiers, int currentMinute) {
+        System.out.println("MINUTO " + currentMinute + " -  - En Cola: " + queue);
+        System.out.print("Caja 1:[" + cashiers[0] + "] | Caja 2:[" + cashiers[1] + "] | Caja 3:[" + cashiers[2] + "] | Caja 4: [" + cashiers[3] + "]");
+    }
+
+    private static int[] sendClientToCashier(int[] cashiers, int queue, int itemsMaximum, int itemsMinimum) {
+        if (queue > 0) {
+            for (int i = 0; i < cashiers.length && queue > 0; i++) {
+                if (cashiers[i] == 0) {
+                    int itemsAsigned = (int) (Math.random() * (itemsMaximum - itemsMinimum) + itemsMaximum);
+                    cashiers[i] = itemsAsigned;
+                    queue--;
+                    return cashiers;
+                }
+            }
+        }
+
+        return cashiers;
+    }
+
+    private static int clientArrives(int queue, double CLIENT_ARRIVAL_PROBABILITY) {
+        double clientArrival = Math.random();
+        if (clientArrival <= CLIENT_ARRIVAL_PROBABILITY) {
+            queue++;
+        }
+        return queue;
 
     }
 
