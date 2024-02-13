0cc3dcbbed5bf6c292c72a7eb22b6b55f5aca732
lydiaa-gr <lydia.garcia@alumnos.uneatlantico.es>
2024-02-13 22:51:12 +0100

    atención en cajas

--- entregas/garciaLydia/reto001/Carrefour.java
+++ entregas/garciaLydia/reto001/Carrefour.java
@@ -12,7 +12,7 @@ public class Carrefour {
         int itemsVendidos = 0;
 
         for (int minuto = 1; minuto <= 720; minuto++) {
-
+           
             if (rand.nextDouble() < 0.6) {
                 cola++;
                 System.out.println("MINUTO " + minuto + " - Llega 1 persona - En Cola: " + cola);
@@ -21,6 +21,15 @@ public class Carrefour {
             }
 
 
-        }
-    }
-}
+            for (int i = 0; i < cajas.length; i++) {
+                if (cajas[i] > 0) {
+                    cajas[i]--;
+                    itemsVendidos++;
+                } else if (cola > 0) {
+                    cajas[i] = rand.nextInt(11) + 5; // Atender a una persona con entre 5 y 15 packs de items
+                    cola--;
+                    personasAtendidas++;
+                }
+            }
+
+
