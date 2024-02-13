48dc014551e9f63b7578e9df62bde4e28f3aaf78
Nazaret <nazaretderas30@gmail.com>
2024-02-13 10:47:40 +0100

    inicio de programa

---
+++ entregas/OrtizNazaret/cccf.java
@@ -0,0 +1,11 @@
+package entregas.OrtizNazaret;
+import java.util.Scanner;
+
+class Carrefour {
+   static int totalPersonsInDay = 0;
+   static int totalItemsSold = 0;
+
+   public static void main(String[] args) {
+        
+   }
+}
