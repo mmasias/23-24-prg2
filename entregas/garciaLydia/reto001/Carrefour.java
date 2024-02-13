60963d28d8e0e434e829cbaf7a8027339d953a54
lydiaa-gr <lydia.garcia@alumnos.uneatlantico.es>
2024-02-13 22:23:39 +0100

    inicio y nombramiento de variables

---
+++ entregas/garciaLydia/reto001/Carrefour.java
@@ -0,0 +1,14 @@
+package entregas.garciaLydia.reto001;
+
+import java.util.Random;
+
+public class Carrefour {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] cajas = new int[4];
+        int cola = 0;
+        int minutosSinCola = 0;
+        int personasAtendidas = 0;
+        int itemsVendidos = 0;
+
+        
