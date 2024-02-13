9b163d365814248d9273ba073aa5ae189fa94349
Andecochea <miguel.andecochea@uneatlantico.es>
2024-02-13 20:16:56 +0100

    desarrollo de las cajas

--- entregas/AndecocheaMiguel/reto-001/CentroComercial.java
+++ entregas/AndecocheaMiguel/reto-001/CentroComercial.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class CentroComercial {
     public static void main(String[] args) {
@@ -32,9 +33,68 @@ public class CentroComercial {
                     totalItems = totalItems + nItems[1];
                     cola--;
 
-        
+                } else if (libre[2]) {
+                    caja[2]++;
+                    libre[2] = false; 
+                    nItems[2] = ((int) (Math.random() * 100)) % 11 + 5;
+                    totalItems = totalItems + nItems[2];
+                    cola--;
+
+                } else if (libre[3]) {
+                    caja[3]++;
+                    libre[3] = false; 
+                    nItems[3] = ((int) (Math.random() * 100)) % 11 + 5;
+                    totalItems = totalItems + nItems[3];
+                    cola--;
+                } else if (libre[4] && activa) {
+                    caja[4]++;
+                    libre[4] = false; 
+                    nItems[4] = ((int) (Math.random() * 100)) % 11 + 5;
+                    totalItems = totalItems + nItems[4];
+                    cola--;
+                }
+            
             }
+            
+            if (nItems[0] > 0) {
+                nItems[0]--;
+                if (nItems[0] == 0) {
+                    libre[0] = true;
+                }
+            }
+            if (nItems[1] > 0) {
+                nItems[1]--;
+                if (nItems[1] == 0) {
+                    libre[1] = true;
+                }
+            }
+
+            if (nItems[2] > 0) {
+                nItems[2]--;
+                if (nItems[2] == 0) {
+                    libre[2] = true;
+                }
+            }
+
+            if (nItems[3] > 0) {
+                nItems[3]--;
+                if (nItems[3] == 0) {
+                    libre[3] = true;
+                }
+            }
+
+            if (nItems[4] > 0) {
+                nItems[4]--;
+                if (nItems[4] == 0) {
+                    libre[4] = true;
+                }
+            }
+            if (cola == 0) {
+                minutosSinCola++;
+            }
+            
         }
+
 
 
     }
