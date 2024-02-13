f8650dfd4a213679ed485be77ee0c80891bcadb6
Romero <rxmerx504@gmail.com>
2024-02-13 11:13:49 +0100

    Declare las varibles que representaran el funcionamiento del tiempo de trabajo, asi como el estado que permitirá que se detenga

--- entregas/romeroAngel/reto-001/reto-001.java
+++ entregas/romeroAngel/reto-001/reto-001.java
@@ -4,6 +4,15 @@ class RetoCarrefour {
 
     public static void main(String[] args) {
         
+        final int CLOSING_TIME = 720;
+        int currentMinute = 0;
+
+        boolean working = currentMinute<CLOSING_TIME;
+
+        do{
+
+        }while(working);
+
     }
 
 }
