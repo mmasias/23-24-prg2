74ed8674a75f68798f17579d11ce94cb1a8706d3
nicolas.lopez <nicolas.lopez@alumnos.uneatlantico.es>
2024-03-05 10:59:10 +0100

    Reinicio del reto

--- entregas/LopezNicolas/reto003/Cine.java
+++ entregas/LopezNicolas/reto003/Cine.java
@@ -1,18 +1,3 @@
 package entregas.LopezNicolas.reto003;
 
-public class Location{
-
-    public Location(String city, String street, int number)
-
-    public String city;
-    public String street;
-    public int number;
-}
-
-public class Cine{
-
-    public String name;
-    public Location location;
-    public int[] rooms;
-    
-}
+ 
