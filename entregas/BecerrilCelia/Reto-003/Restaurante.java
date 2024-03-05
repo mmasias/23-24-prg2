f0db4caec49e5040ea7dedd240837d55055d5ae0
Celia Becerril Riaño <144801825+celiabecerril@users.noreply.github.com>
2024-03-05 11:04:50 +0100

    Clases completas. Metodos extra

--- entregas/BecerrilCelia/Reto-003/Restaurante.java
+++ entregas/BecerrilCelia/Reto-003/Restaurante.java
@@ -5,9 +5,21 @@ import java.time.LocalDateTime;
 class Restaurante {
 
     private Cliente cliente;
+    private Mesa[] mesa;
     private Reserva reserva;
-    
-    
+    private int indiceMesa;
+    private Reserva[] reservas;
+    private int indiceReserva;
+
+    public Restaurante(Cliente cliente, Mesa mesa, Reserva reserva, int maxMesas, int maxReservas) {
+        this.cliente = cliente;
+        this.mesa = new Mesa[maxMesas];
+        this.reserva = reserva;
+        this.indiceMesa = 0;
+        this.reservas = new Reserva[maxReservas];
+        this.indiceReserva = 0;
+    }
+
     public Cliente getCliente() {
         return cliente;
     }
@@ -23,9 +35,60 @@ class Restaurante {
     public void setReserva(Reserva reserva) {
         this.reserva = reserva;
     }
-    
-}
 
+    public void anadirMesa(Mesa mesa) {
+        if (indiceMesa < this.mesa.length) {
+            this.mesa[indiceMesa] = mesa;
+            indiceMesa++;
+        } else {
+            System.out.println("No se puede añadir más mesas. El restaurante está lleno.");
+        }
+    }
+
+    public void removeMesa(Mesa mesa) {
+        for (int i = 0; i < indiceMesa; i++) {
+            if (this.mesa[i].equals(mesa)) {
+                for (int j = i; j < indiceMesa - 1; j++) {
+                    this.mesa[j] = this.mesa[j + 1];
+                }
+                indiceMesa--;
+                return;
+            }
+        }
+        System.out.println("La mesa no se encontró.");
+    }
+
+    public void asignarClienteAMesa(int numeroMesa, Cliente cliente) {
+        if (numeroMesa >= 0 && numeroMesa < mesa.length) {
+            mesa[numeroMesa].setCliente(cliente);
+        } else {
+            System.out.println("Número de mesa inválido.");
+        }
+    }
+
+    public void addReserva(Reserva reserva) {
+        if (indiceReserva < reservas.length) {
+            this.reservas[indiceReserva] = reserva;
+            indiceReserva++;
+        } else {
+            System.out.println("No se puede añadir más reservas. El restaurante está lleno.");
+        }
+    }
+
+    public void removeReserva(Reserva reserva) {
+        for (int i = 0; i < indiceReserva; i++) {
+            if (reservas[i].equals(reserva)) {
+                for (int j = i; j < indiceReserva - 1; j++) {
+                    reservas[j] = reservas[j + 1];
+                }
+                indiceReserva--;
+                return;
+            }
+        }
+        System.out.println("La reserva no se encontró.");
+    }
+
+}
 
 class Cliente {
 
@@ -53,8 +116,16 @@ class Cliente {
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-}
 
+    public void hacerReserva(Restaurante restaurante, Mesa mesa, LocalDateTime fechaHora) {
+        Reserva reserva = new Reserva(this, mesa, fechaHora);
+        restaurante.addReserva(reserva);
+    }
+
+    public void anularReserva(Restaurante restaurante, Reserva reserva) {
+        restaurante.removeReserva(reserva);
+    }
+}
 
 class Reserva {
 
@@ -86,9 +157,22 @@ class Reserva {
         this.mesa = mesa;
     }
 
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public boolean estaCancelada() {
+        return estaCancelada;
+    }
+
+    public void setEstaCancelada(boolean estaCancelada) {
+        this.estaCancelada = estaCancelada;
+    }
 }
-
-
 
 class Mesa {
 
@@ -119,14 +203,27 @@ class Mesa {
         this.numero = numero;
     }
 
+    public boolean estaDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+
+    public void setReserva(Reserva reserva) {
+        if (reserva != null) {
+            this.disponible = false;
+        } else {
+            this.disponible = true;
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
