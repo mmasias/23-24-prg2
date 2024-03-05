40fd7f8ee3421661999c0227e0b2054fa72406ee
Martin <martin.lopez@alumnos.uneatlantico.es>
2024-03-05 11:12:55 +0100

    creacion de constructores y objeto de cada clase

--- entregas/lopezMartin/reto-003/Cine.java
+++ entregas/lopezMartin/reto-003/Cine.java
@@ -1,12 +1,55 @@
 class Cine{
 
+    public Cine(String nombreCine, String direccion){
+
+    }
+
+    Cine cine1 = new Cine("Cinesa", "C. Francisco Tomás y Valiente");
+
+    
+
 }
 class Pelicula{
+
+    public Pelicula(String nombrePelicula, String horaSesionPelicula, int salaEstablecida){
+
+    }
+
+    Pelicula starWars1 = new Pelicula("Star Wars", "20:45", 3);
 
 }
 class Ticket{
 
+    public Ticket(String nombrePelicula, String sesionPelicula, int precio, boolean ticketValidad){
+
+    }
+
+    Ticket ticket1 = new Ticket("Star Wars", "20:45", 7, false);
+
+
 }
 class Visitante{
+
+    public Visitante(boolean tieneTciket){
+
+    }
+
+    Visitante visitante1 = new Visitante(true);
+
+    public void comprarTicket(){
+
+    }
+
+    public void enseñarTicket(){
+
+    }
+    public void entrarSala(){
+
+    }
     
 }
+
+
+
+
+
