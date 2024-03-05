60b78b8e18876b3b0db9dcb691bc3b4ab453f616
nicolas.lopez <nicolas.lopez@alumnos.uneatlantico.es>
2024-03-05 11:08:44 +0100

    Creación de las clases 'Cinema' y 'Movie'

--- entregas/LopezNicolas/reto003/Cine.java
+++ entregas/LopezNicolas/reto003/Cine.java
@@ -1,3 +1,21 @@
 package entregas.LopezNicolas.reto003;
 
- 
+ class Cinema {
+
+    public Cinema(String name, Location location);
+
+    public void open();
+    public void open(Date date);
+    public void open(Intervalo time);
+    public void closed();
+    public boolean nowOpen();
+    public String[] billboard(Movie movie);
+    public int rooms();
+
+ }
+
+ class Movie {
+
+    public Movie (String title, String director, long duration);
+    
+ }
