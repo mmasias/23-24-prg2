34bef49fb129aec77b639bad951206e975523ad3
andersonliam873 <liam.anderson@uneatlantico.es>
2024-03-06 19:30:27 +0100

    RETO 3

---
+++ entregas/andersonLiam/reto-003/Cine.java
@@ -0,0 +1,15 @@
+public class Cine{
+private Pelicula[] pelicula;
+private Visitante[] visitante;
+private Tiket[] tiket;
+
+public Cine(Pelicula[] pelicula, Visitante[] visitante, Tiket[] tiket) {
+    this.pelicula = pelicula;
+    this.visitante = visitante;
+    this.tiket = tiket;
+}
+
+
+
+
+}
---
+++ entregas/andersonLiam/reto-003/Visitante.java
@@ -0,0 +1,17 @@
+public class Visitante {
+    private String nombre;
+    private String apellido;
+    private int numeroDeIndentidad;
+    
+    
+    public Visitante(String nombre, String apellido, int numeroDeIndentidad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroDeIndentidad = numeroDeIndentidad;
+    }
+
+
+   
+   
+
+}
