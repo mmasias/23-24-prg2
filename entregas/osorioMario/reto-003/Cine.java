5741cfb9f7f3fb0cc28875cc8454df06abc0c836
Maritoz <maritox47@gmail.com>
2024-03-07 21:45:12 +0100

    Cine

---
+++ entregas/osorioMario/reto-003/Cine.java
@@ -0,0 +1,14 @@
+public class Cine {
+    Pelicula pelicula;
+    Ticket ticket;
+    Visitante visitante;
+
+    public Cine(Pelicula pelicula, Ticket ticket, Visitante visitante) {
+        pelicula = pelicula;
+        ticket = ticket;
+        visitante = visitante;
+    }
+
+    public void iniciar() {
+    }
+}
