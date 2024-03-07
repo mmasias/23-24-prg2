c2c89db86ba9793eb543670dc543e16cf25327c7
Sergiorl2 <145299767+Sergiorl2@users.noreply.github.com>
2024-03-07 19:56:17 +0100

    Primer commit
    Contiene las clases principales y algún método

---
+++ entregas/Biblioteca.java
@@ -0,0 +1,36 @@
+class Biblioteca {
+
+public Biblioteca() {
+}
+
+
+class Libro {
+    
+}
+
+public void agregarLibro(Libro libro) {
+    
+}
+
+
+class Usuario {
+    
+}
+
+public void agregarUsuario(Usuario usuario) {
+    
+}
+
+
+class Prestamo {
+    
+}
+
+public void agregarPrestamo(Prestamo prestamo) {
+    
+}
+
+
+
+
+}
