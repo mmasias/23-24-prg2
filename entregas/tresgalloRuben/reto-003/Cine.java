635ef1ae541ef2682b15d1be479b216d18f3dee6
rubentresgallob <ruben.tresgallo@alumnos.uneatlantico.es>
2024-03-07 15:00:22 +0100

    clase cine

---
+++ entregas/tresgalloRuben/reto-003/Cine.java
@@ -0,0 +1,15 @@
+public class Cine{
+    public Pelicula pelicula;
+    public Tiket tiket;
+    public Visitante visitante;
+    
+    
+    public Cine(Pelicula pelicula, Visitante visitante, Tiket tiket) {
+        this.pelicula = pelicula;
+        this.tiket = tiket;
+        this.visitante = visitante;
+        
+    }
+
+
+}
