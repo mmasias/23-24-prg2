6f608755b1cee722c54943e592ad6144da4dd4f4
JPMorales04 <jpmorales.epic@gmail.com>
2024-03-07 19:28:27 +0100

    Reto-003

---
+++ entregas/MoralesJosePablo/reto-003/Cinesa.java
@@ -0,0 +1,34 @@
+class Cinesa {
+    public Cinesa(String nombre, String direccion)
+
+    public String getNombre()
+    public void setNombre(String nombre)
+    public String getDireccion()
+    public void setDireccion(String direccion)
+}
+class Pelicula {
+    public Pelicula(String titulo, int duracion)
+
+    public String getTitulo()
+    public void setTitulo(String titulo)
+    public int getDuracion()
+    public void setDuracion(int duracion)
+}
+class Ticket {
+    public Ticket(int numeroTicket, double precio)
+
+    public int getNumeroTicket()
+    public void setNumeroTicket(int numeroTicket)
+    public double getPrecio()
+    public void setPrecio(double precio)
+}
+
+
+class Visitante {
+    public Visitante(String nombre, int edad)
+
+    public String getNombre()
+    public void setNombre(String nombre)
+    public int getEdad()
+    public void setEdad(int edad)
+}
