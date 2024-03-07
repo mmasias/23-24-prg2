ab2c135128e820f0aacb8fdab1f984b7c91f5662
Sergiorl2 <145299767+Sergiorl2@users.noreply.github.com>
2024-03-07 20:08:34 +0100

    Constructores
    Añadimos constructores en cada clase

--- entregas/Biblioteca.java
+++ entregas/Biblioteca.java
@@ -1,36 +1,39 @@
 class Biblioteca {
 
-public Biblioteca() {
-}
+    public Biblioteca(String nombre, String ciudad, LocalTime horaApertura, LocalTime horaCierre) {
 
 
-class Libro {
-    
+
 }
+}
+
+class Libro {     
+
+    public Libro(String titulo, String autor, int año, String editorial, String genero, boolean disponible) {
+       
+       
+        
+    }
 
 public void agregarLibro(Libro libro) {
     
 }
-
+}
 
 class Usuario {
     
-}
+    public Usuario(String nombre, int numeroID)
 
 public void agregarUsuario(Usuario usuario) {
     
 }
-
+}
 
 class Prestamo {
     
-}
+    public Prestamo(String nombreUsuario, String nombreLibro, int diaEntregaLibro, int diaDevolucionLibro)
 
 public void agregarPrestamo(Prestamo prestamo) {
     
 }
-
-
-
-
 }
