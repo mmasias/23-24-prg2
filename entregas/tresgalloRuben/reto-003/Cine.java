f492c4a3192dde733c0f2f614b3481871fa2e8f5
rubentresgallob <ruben.tresgallo@alumnos.uneatlantico.es>
2024-03-07 15:14:54 +0100

    ticket

--- entregas/tresgalloRuben/reto-003/Cine.java
+++ entregas/tresgalloRuben/reto-003/Cine.java
@@ -1,15 +1,15 @@
 public class Cine{
     public Pelicula pelicula;
-    public Tiket tiket;
+    public Ticket ticket;
     public Visitante visitante;
     
     
-    public Cine(Pelicula pelicula, Visitante visitante, Tiket tiket LocalDate apertura, LocalDate cierre){
+    public Cine(Pelicula pelicula, Visitante visitante, Ticket ticket LocalDate apertura, LocalDate cierre){
 
         public boolean abierto()
 
         this.pelicula = pelicula;
-        this.tiket = tiket;
+        this.ticket = ticket;
         this.visitante = visitante;
         
     }
