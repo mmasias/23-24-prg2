375318264b678986f550aebfcf9b87823312b5e6
Lukrecio <Lucamegalodon@gmail.com>
2024-03-07 19:21:43 +0100

    reto hecho

--- retosvarios/carrefour.java
+++ retosvarios/carrefour.java
@@ -1,38 +1,38 @@
-package retosvarios;
-
-class carrefour {
-    public static void main(String[] args) {
-
-        for (int hTime = 9; hTime <= 21; hTime++) {
-            for (int timeMinutes = 0; timeMinutes <= 24; timeMinutes++) {
-                boolean Client = Math.random() < 0.6;
-                boolean[] reception = { false, false, false, false };
-                // arrivalClient(Client, reception);
-                for(int column = 0;column<reception.length;column++) {
-                    System.out.print(reception[column] + " ");
-                } System.out.println();
-
-            }
-        }
-
-    }
-
-    // private static boolean arrivalClient(boolean client, boolean[] reception) {
-    //     if (client == true && reception[0] == false) {
-    //         reception[0] = true;
-    //         System.out.println("La caja 1 está ocupada");
-    //         double[] countdown = { 0, 0, 0, 0 };
-    //         while (reception[0] = true) {
-    //             double clientTime = Math.random() * (15 - 5 + 1) + 5;
-    //             countdown[0]++;
-    //             if (countdown[0] == clientTime) {
-    //                 reception[0] = false;
-    //                 System.out.println("La caja 1 se ha liberado");
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // }
-
-}
+//package retosvarios;
+//
+//class carrefour {
+//    public static void main(String[] args) {
+//
+//        for (int hTime = 9; hTime <= 21; hTime++) {
+//            for (int timeMinutes = 0; timeMinutes <= 24; timeMinutes++) {
+//                boolean Client = Math.random() < 0.6;
+//                boolean[] reception = { false, false, false, false };
+//                // arrivalClient(Client, reception);
+//                for(int column = 0;column<reception.length;column++) {
+//                    System.out.print(reception[column] + " ");
+//                } System.out.println();
+//
+//            }
+//        }
+//
+//    }
+//
+//    // private static boolean arrivalClient(boolean client, boolean[] reception) {
+//    //     if (client == true && reception[0] == false) {
+//    //         reception[0] = true;
+//    //         System.out.println("La caja 1 está ocupada");
+//    //         double[] countdown = { 0, 0, 0, 0 };
+//    //         while (reception[0] = true) {
+//    //             double clientTime = Math.random() * (15 - 5 + 1) + 5;
+//    //             countdown[0]++;
+//    //             if (countdown[0] == clientTime) {
+//    //                 reception[0] = false;
+//    //                 System.out.println("La caja 1 se ha liberado");
+//    //                 return true;
+//    //             }
+//    //         }
+//    //     }
+//    //     return false;
+//    // }
+//
+//}
