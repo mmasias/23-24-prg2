c9bc94ebd833f7c4dd8d6f92c654dec5b0555c7e
pareyor <pablo.reyortiz@alumnos.uneatlantico.es>
2024-03-07 08:59:05 +0100

    Clase Visitante, final

--- entregas/reyPablo/Reto-003/Visitante.java
+++ entregas/reyPablo/Reto-003/Visitante.java
@@ -1,4 +1,47 @@
 
-public class Visitante {
+    public class Visitante {
+        private String nombre;
+        private String apellido;
+        private int numeroDeIndentidad;
+        
+        
+        public Visitante(String nombre, String apellido, int numeroDeIndentidad) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.numeroDeIndentidad = numeroDeIndentidad;
+        }
     
-}
+    
+        public String getNombre() {
+            return nombre;
+        }
+    
+    
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+    
+    
+        public String getApellido() {
+            return apellido;
+        }
+    
+    
+        public void setApellido(String apellido) {
+            this.apellido = apellido;
+        }
+    
+    
+        public int getNumeroDeIndentidad() {
+            return numeroDeIndentidad;
+        }
+    
+    
+        public void setNumeroDeIndentidad(int numeroDeIndentidad) {
+            this.numeroDeIndentidad = numeroDeIndentidad;
+        }
+        
+        
+       
+    
+    }
