49ed17d168f3b4fe4988f5c53fa20cb115301c13
OscarPineda9 <oscar.pineda@alumnos.uneatlantico.es>
2024-03-07 23:48:40 +0100

    Quinto y Ultimo Commit
    Formatting Document y chequeando por posible errores.

--- entregas/Reto03/Cine.java
+++ entregas/Reto03/Cine.java
@@ -1,54 +1,96 @@
 package entregas.Reto03;
 
 class Cine {
-    public Cine(String nombre,int horario){}
+    public Cine(String nombre, int horario) {
+    }
 
-    public void abierto(){}
-    public void estaAbierto(){}
-    public void cerrado(){}
-    public void estaCerrado(){}
-    public void mostrarPeliculas(){}
-    public Ticket comprarTicket(){}
+    public void abierto() {
+    }
 
-    
-}
-class Ticket{
-    
-    public Ticket(String persona, int edad){}
-    
-    public Pelicula pelicula(){}
-    public boolean promociones(String pelicula){}
-    public String[ ][ ] asientos(String fila, int numeroAsiento){}
-    public boolean asientosEspeciales(){}
-    public boolean snacks(){}
-    public void precio(){}
-    public void pago(){}
-    public Persona persona(){}
-}
+    public void estaAbierto() {
+    }
 
-class Pelicula{
+    public void cerrado() {
+    }
 
-    public Pelicula(String titulodePelicula,double duracionDePelicula){}
+    public void estaCerrado() {
+    }
 
-    public void titulodePelicula(){}
-    public double horario(){}
-    public double duracionDePelicula(){}
-    public boolean estaDisponible(){}
-    public void fecha(int fecha){}
+    public void mostrarPeliculas() {
+    }
+
+    public Ticket comprarTicket() {
+    }
 
 }
 
+class Ticket {
 
-class Persona{
+    public Ticket(String persona, int edad) {
+    }
 
-    public Persona(String nombre){}
+    public Pelicula pelicula() {
+    }
 
-    public void nombre(){}
-    public void edad(){}
-    public void dni(){}
-    public void genero(){}
+    public boolean promociones(String pelicula) {
+    }
+
+    public String[][] asientos(String fila, int numeroAsiento) {
+    }
+
+    public boolean asientosEspeciales() {
+    }
+
+    public boolean snacks() {
+    }
+
+    public void precio() {
+    }
+
+    public void pago() {
+    }
+
+    public Persona persona() {
+    }
+}
+
+class Pelicula {
+
+    public Pelicula(String titulodePelicula, double duracionDePelicula) {
+    }
+
+    public void titulodePelicula() {
+    }
+
+    public double horario() {
+    }
+
+    public double duracionDePelicula() {
+    }
+
+    public boolean estaDisponible() {
+    }
+
+    public void fecha(int fecha) {
+    }
 
 }
 
+class Persona {
 
+    public Persona(String nombre) {
+    }
 
+    public void nombre() {
+    }
+
+    public void edad() {
+    }
+
+    public void dni() {
+    }
+
+    public void genero() {
+    }
+
+}
