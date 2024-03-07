2bcf12ca1a98377b5149a2523eb5537d0eb11f71
andreslb9 <144810780+andreslb9@users.noreply.github.com>
2024-03-07 22:55:43 +0100

    Tercer Commit

--- entregas/lopezAndres/reto-003/Restaurante.java
+++ entregas/lopezAndres/reto-003/Restaurante.java
@@ -38,3 +38,18 @@ public class Interval{
 
 }
 
+public class Main {
+
+    public static void main(String[] args) {
+        Interval schedule=new createSchedule(16, 23);
+        Restaurant restaurant=new restaurant(schedule);
+
+        Client jaimeLinares=new Client("Jaime Linares");
+        Table table1=new Table(1,5);
+        boolean tableAvailable=table1.available();
+        if(tableAvailable){
+            Booking jaimeLinaresReserve=new Booking(jaimeLinares,"06/03/2024","22:00",table1);
+        }
+    }
+} 
+
