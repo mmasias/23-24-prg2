98166236a30a570d0d368a4bdd7c422b5b20e94f
alejandrofondo <jose.fondo@alumnos.uneatlantico.es>
2024-03-07 18:55:23 +0100

    Reto realizado

--- entregas/fondoAlejandro/Restaurante.java
+++ entregas/fondoAlejandro/Restaurante.java
@@ -3,11 +3,10 @@ class Restaurante {
     public Restaurante(Fecha apertura, Fecha cierre)
 
     public boolean abierto()
-    public Reserva reservarMesa(Cliente cliente)
-    public Mesa adjudicarMesa(Reserva reserva)
+    public Reserva reservarMesa(Reserva reserva)
+    public Mesa adjudicarMesa(Cliente cliente)
     public Mesa mesaOcupada()
     public void atenderCliente(Cliente nombre)
-
 }
 
 class Reserva {
@@ -25,4 +24,22 @@ class Mesa {
 class Cliente {
     
     public Cliente(String nombre)
+}
+
+class Simular {
+
+    public static void main(String [] args) {
+        
+        Restaurante donJamon = new Restaurante (apertura, cierre);
+
+        Cliente fondoAlejandro = new Cliente ("Alejandro Fondo");
+        donJamon.reservarMesa(fondoAlejandro);
+
+        Mesa unaMesa = new Mesa(1,2);
+
+        if(!mesaOcupada(1,2)) {
+            unaMesa.adjudicarMesa(fondoAlejandro);
+            unaMesa.mesaOcupada(1,2);
+        }
+    }
 }
