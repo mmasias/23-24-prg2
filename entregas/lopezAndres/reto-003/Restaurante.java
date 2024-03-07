b1f0fd1b428ea661e4f31d3e752fb94d4acde24f
andreslb9 <144810780+andreslb9@users.noreply.github.com>
2024-03-07 22:49:36 +0100

    Primer Commit

--- entregas/lopezAndres/reto-003/Restaurante.java
+++ entregas/lopezAndres/reto-003/Restaurante.java
@@ -1,1 +1,23 @@
+public class Restaurant {
 
+    public boolean isOpen(Interval openSchedule);
+    public Restaurant(Interval openSchedule);
+
+}
+
+public class Cliente {
+
+    public String name();
+    public String surname();
+    public Client(String name, String surname);
+
+}
+
+public class Booking {
+
+    public Booking(Client client, String date, Stering time, Table table);
+    public String date();
+    public String time();
+    public boolean inSchedule(Interval schedule);
+
+}
