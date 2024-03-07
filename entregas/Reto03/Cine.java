daee187263718bf5727a345aacc3fb2fed0b8161
OscarPineda9 <oscar.pineda@alumnos.uneatlantico.es>
2024-03-07 23:21:48 +0100

    Primer Commit
    Creacion de la primera clase:Cine

---
+++ entregas/Reto03/Cine.java
@@ -0,0 +1,15 @@
+package entregas.Reto03;
+
+class Cine {
+    public Cine(String nombre,int horario){}
+
+    public void abierto(){}
+    public void estaAbierto(){}
+    public void cerrado(){}
+    public void estaCerrado(){}
+    public void mostrarPeliculas(){}
+    public Ticket comprarTicket(){}
+
+    
+    
+}
