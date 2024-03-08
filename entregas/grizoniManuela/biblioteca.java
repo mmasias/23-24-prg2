4130428997ec96755b343020bceb6175e56f49fc
ManuelaGrzoni <manuela.grizoni@alumnos.uneatlantico.es>
2024-03-08 16:37:55 +0100

    Commit 1(reto003)

---
+++ entregas/grizoniManuela/biblioteca.java
@@ -0,0 +1,19 @@
+package entregas.grizoniManuela;
+
+public class biblioteca {
+
+    private String nombre;
+    private String direccion;
+    private int capacidadBiblioteca;
+
+    public biblioteca(String nombre, String direccion, int capacidadBiblioteca){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.capacidadBiblioteca = capacidadBiblioteca;
+        
+
+    }
+    
+    
+}
+
