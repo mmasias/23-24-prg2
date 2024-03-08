1fd505a8a9cd3462192cd804f435028e819d1681
Manuel Masías <mmasias@users.noreply.github.com>
2024-03-08 11:36:16 +0100

    Update Reserva.java

--- entregas/juarezAlejandro/reto-003/Reserva.java
+++ entregas/juarezAlejandro/reto-003/Reserva.java
@@ -1,6 +1,5 @@
 class Reserva {
-    public Reserva(int diaReserva);
-    public void nombrePersonaReserva();
-    public void identificacionPersonaReserva();
+    public Reserva(int diaReserva, Cliente cliente);
+    
     public String obtenerNumeroReserva();
 }
