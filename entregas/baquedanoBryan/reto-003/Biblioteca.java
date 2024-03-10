4da2ba149e36aeca3531f6f5b0e3ebd03430667f
enmabry <bryanbaquedano11@gmail.com>
2024-03-10 22:40:25 +0100

    Detalles, como arreglo de mayúsculas, y eliminación de getters.

--- entregas/baquedanoBryan/reto-003/Biblioteca.java
+++ entregas/baquedanoBryan/reto-003/Biblioteca.java
@@ -1,15 +1,12 @@
 class Usuario {
     
-    public Usuario(String Nombre, int DNI);
-    public Usuario(int DNI);
-
-    public String obtenerNombre();
-    public int obtenerDNI();
+    public Usuario(String Nombre, int dni);
+    public Usuario(int dni);
 }
 
 class Libro {
 
-    public Libro(String obtenerTítulo, String Autor);
+    public Libro(String obtenerTítulo, String autor);
     public Libro(String obtenerTítulo);
     
     public String obtenerTítulo();
@@ -20,19 +17,19 @@ class Libro {
 
 class Préstamo{
 
-    class Préstamo(String nombreUsuario, int DNI, int DíaQueSeHiZO, Int DíaDevolución, String nombreLibro )
-
+    class Préstamo(String nombreUsuario, int dni, int DíaQueSeHiZO, int DíaDevolución, String nombreLibro );
     class int obtenerIdentificador();
 
 }
 
+
 class Biblioteca{ 
 
-class Biblioteca(String Ubicación);
+    class Biblioteca(String Ubicación);
 
-class void hacerPréstamo(Préstamo prestamo);
-class boolean operativa(Fecha fecha);
-class boolean operativa(Hora hora);
-class void aplicarMulta(Préstamo préstamo);
-class void actualizarDisponilidad();
+    class void hacerPréstamo(Préstamo prestamo);
+    class boolean operativa(Fecha fecha);
+    class boolean operativa(Hora hora);
+    class void aplicarMulta(Préstamo préstamo);
+    class void actualizarDisponilidad();
 }
