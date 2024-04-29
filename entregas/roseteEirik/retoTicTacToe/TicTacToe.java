68c36d7cd0352504c278f1f482ac5b33e6d1eee5
eirik.rosete <eirik.rosete@gmail.com>
2024-04-29 12:53:53 +0200

    Implementación del reto TicTacToe
    Se implementó un nuevo reto que consiste en usar el método HOOD visto en clase.
    
    Asimismo, se implementó el mismo ejercicio que se vio en clase, por partes y desde un inicio para abarcarlo

---
+++ entregas/roseteEirik/retoTicTacToe/TicTacToe.java
@@ -0,0 +1,14 @@
+package entregas.roseteEirik.retoTicTacToe;
+
+class TicTacToe {
+    
+    public void play(){
+
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.play();
+    }
+
+}
