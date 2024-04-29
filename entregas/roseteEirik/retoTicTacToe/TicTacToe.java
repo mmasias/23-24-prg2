d83f0d44a8b0b395d55a3014b7456d7ddf1b4613
eirik.rosete <eirik.rosete@gmail.com>
2024-04-29 13:06:22 +0200

    Se implementó la vista privada de la clase TicTacToe
    Se creó el tablero, los jugadores y la mecánica de turnos para que así, más tarde se creen las clases correspondientes para que funcione el código

--- entregas/roseteEirik/retoTicTacToe/TicTacToe.java
+++ entregas/roseteEirik/retoTicTacToe/TicTacToe.java
@@ -1,14 +1,21 @@
 package entregas.roseteEirik.retoTicTacToe;
 
 class TicTacToe {
-    
-    public void play(){
 
+    private Board board;
+    private Player[] players;
+    private Turn turn;
+
+    public void play() {
+        board = new Board();
+        players = new Player[2];
+        players[0] = new Player['X'];
+        players[1] = new Player['O'];
+        turn = new Turn();
     }
 
     public static void main(String[] args) {
         TicTacToe game = new TicTacToe();
         game.play();
     }
-
 }
