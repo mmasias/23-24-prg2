8f31c7deb01551a2e92e49739986df10b21729af
antoniogavilanes <antonio.gavilanes@alumnos.uneatlantico.es>
2024-05-03 12:06:34 +0200

    Baraja

--- entregas/Solitario/Baraja.java
+++ entregas/Solitario/Baraja.java
@@ -1,13 +1,52 @@
 
 public class Baraja {
-    
-    public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+
+    private Carta[] cartas;
+    private int ultima;
+
+    public Baraja() {
+
+        cartas = new Carta[52];
+        for (int palo=0; palo<4; palo++) {
+            for (int numero=1; numero<=13; numero++) {
+                this.poner(new Carta(palo, numero));
+            }
+        }
+
+        this.barajar();
     }
+    
+    private void barajar() {
+
+        Random aleatorio = new Random();
+        for (int i=0; i<1000; i++) {
+            int origen = aleatorio.nextInt(52);
+            int destino = aleatorio.nextInt(52);
+            Carta carta = cartas[origen];
+            cartas[origen] = cartas[destino];
+            cartas[destino] = carta;
+        }
+    }
+
+    private void poner(Carta carta) {
+        cartas[ultima] = carta;
+        ultima++;
+
+    }
+
+    public void mostrar() {
+
+        System.out.println("Baraja:");
+        Carta carta = this.cima();
+        carta.mostrar();
+    }
+    private Carta cima() {
+        return cartas[ultima-1];
+    }
+
     public void moverA(Descarte descarte) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'moverA'");
+        
+
     }
 
 
---
+++ entregas/Solitario/Carta.java
@@ -0,0 +1,13 @@
+
+public class Carta {
+
+    public Carta(int palo, int numero) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void mostrar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+    }
+
+}
